188ee4029169067c39070b3c58bfbdba2c18dfa1
Aryan Shaw <aryan4p130202@gmail.com>
2021-09-20 01:09:05 +0530

    Create invert-using-stack.cpp

---
+++ invert-binary-tree/invert-using-stack.cpp
@@ -0,0 +1,14 @@
+TreeNode* invertTree(TreeNode* root) {
+    std::stack<TreeNode*> stk;
+    stk.push(root);
+    
+    while (!stk.empty()) {
+        TreeNode* p = stk.top();
+        stk.pop();
+        if (p) {
+            stk.push(p->left);
+            stk.push(p->right);
+            std::swap(p->left, p->right);
+        }
+    }
+    return root;
