cf5b2cca9b79617cabcccc24a3f8e3750ef40660
Aryan Shaw <aryan4p130202@gmail.com>
2021-09-29 19:04:40 +0530

    Time: 4 ms (49.57%), Space: 9.7 MB (78.17%) - LeetHub

--- invert-binary-tree/invert-binary-tree.cpp
+++ invert-binary-tree/invert-binary-tree.cpp
@@ -12,10 +12,10 @@
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-      if(root==NULL)return 0;
-        invertTree(root->right);
+        if(root==NULL)return NULL;
         invertTree(root->left);
-        swap(root->right,root->left);
+        invertTree(root->right);
+        swap(root->left,root->right);
         return root;
     }
 };
