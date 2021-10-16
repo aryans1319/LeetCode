dd93c426cff3a74e2f4773a4f6920876aab102d0
Aryan Shaw <aryan4p130202@gmail.com>
2021-10-16 10:56:47 +0530

    Time: 4 ms (57.91%), Space: 8 MB (84.26%) - LeetHub

--- unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -3,17 +3,33 @@ public:
     bool uniqueOccurrences(vector<int>& arr) {
         unordered_map<int,int>mp;
         bool flag;
-        set<int>s;
+        unordered_set<int>s;
+        // Count Frequency of each elements in a Map
         for(auto i:arr){
             mp[i]++;
         }
-        for(auto it:mp){
-            s.insert(it.second);
-            if(s.size()==mp.size())
-                flag=true;
-            else
-                flag=false;
-        }
-     return flag;
+        /* Iterate through the map and insert the key's value pair in a set.
+            We are doing this so that it stores only unique values which set does.
+            
+            For eg: Look at Example 1, 1->3
+                                       2->2 
+                                       3->1
+                                       insert the values in set s[1,2,3](values in set)
+                                       compare them with map size they are equal hence mark
+                                       flag as true.
+                    
+                    Example 2,         1->1
+                                       2->1
+                                       
+                                       if we insert values of key in set it would store only 1
+                                       (because set stores only unique values) therefore set size
+                                       equals 1 but mp has size 2 so mark flag as false.
+         
+         I hope it's clear now if you any doubt comment them below.                                       
+         */
+
+  for (auto& p : mp)
+      if (!s.insert(p.second).second) return false;
+  return true;
     }
 };
