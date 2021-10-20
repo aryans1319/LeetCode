95931a89880a043a92981d1c549e887cc5ebb95f
Aryan Shaw <aryan4p130202@gmail.com>
2021-10-20 14:53:38 +0530

    Time: 8 ms (49.83%), Space: 10.6 MB (28.48%) - LeetHub

---
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+      map<int,int>mp;
+        vector<int>ans;
+       for(int i=0;i<nums1.size();i++){
+           mp[nums1[i]]++;
+       } 
+        for(int i=0;i<nums2.size();i++){
+            if(mp[nums2[i]]>0){
+                mp[nums2[i]]--;
+                ans.push_back(nums2[i]);
+        }
+        }
+        return ans;
+    }
+};
