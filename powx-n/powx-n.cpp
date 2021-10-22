c1c261e723bcdb23a86ba8cfc5b5e5fe155a2563
Aryan Shaw <aryan4p130202@gmail.com>
2021-10-22 12:42:00 +0530

    Time: 0 ms (100.00%), Space: 6 MB (43.09%) - LeetHub

--- powx-n/powx-n.cpp
+++ powx-n/powx-n.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
+    double help(double x,int n){
+        if(n==0) return 1;
+        double ans=help(x,n/2);
+        ans*=ans;
+        if(n%2==1){
+            ans*=x;
+        }
+        return ans;
+    }
+    
     double myPow(double x, int n) {
-       if(n==0) return 1;
-        
-        double y = myPow(x,n/2);
-        if(n%2==0){
-            
-            return y*y;
-            
-        }
-        else{
-            return n < 0 ? 1/x*y*y : x*y*y; 
-        }
+        if(n==INT_MIN) n=INT_MIN+2;
+        double ans=help(x,abs(n));
+        if(n<0) return 1/ans;
+        else return ans;
     }
 };
