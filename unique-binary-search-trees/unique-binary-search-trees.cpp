7826f55e3d369ea1c3788abc418336fd469652cf
Aryan Shaw <aryan4p130202@gmail.com>
2021-11-09 00:29:51 +0530

    Time: 0 ms (100.00%), Space: 6 MB (37.05%) - LeetHub

---
+++ unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+   int solve(int n, vector<int>&dp){
+        if(n <= 1) return 1;
+        if(dp[n] != -1) return dp[n];
+        
+        int res{};
+        
+        for(int i{1}; i<=n; ++i)
+            res += solve(i-1, dp)*solve(n-i, dp);
+        
+        return dp[n] = res;
+    }
+    
+    int numTrees(int n) {
+        vector<int>dp(n+1, -1);
+        return solve(n, dp);
+    }
+};
