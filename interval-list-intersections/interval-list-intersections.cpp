e2478e660d14659da0f6927b97bc5de8f643f911
Aryan Shaw <aryan4p130202@gmail.com>
2021-11-25 00:43:14 +0530

    Time: 49 ms (19.00%), Space: 18.8 MB (43.66%) - LeetHub

---
+++ interval-list-intersections/interval-list-intersections.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& A, vector<vector<int>>& B) {
+          vector<vector<int>> ans;
+        int i = 0, j = 0;
+        while(i < size(A) && j < size(B)) {
+			// intersection condition
+            if(A[i][1] >= B[j][0] && A[i][0] <= B[j][1]) 
+                ans.emplace_back(initializer_list<int>{ max(A[i][0], B[j][0]), min(A[i][1], B[j][1]) });
+            A[i][1] < B[j][1] ? i++ : j++;    // increment pointer of list whose interval ended first
+        }
+        return ans;
+    }
+};
