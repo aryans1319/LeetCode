305fc6ea8a7fa514e3f2beeecb2f95d4704e1511
Aryan Shaw <aryan4p130202@gmail.com>
2021-11-25 15:23:37 +0530

    Time: 921 ms (5.01%), Space: 269.5 MB (5.95%) - LeetHub

---
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int bestTime(vector<int>&prices,int currentDay,bool canBuy,int transCount,vector<vector<vector<int>>>&dp){
+        if(currentDay >= prices.size() || transCount<=0){
+            return 0;
+        }
+        
+        if(dp[currentDay][canBuy][transCount]!=-1)
+        {
+            return dp[currentDay][canBuy][transCount];
+        }
+        
+        int idle = bestTime(prices, currentDay+1, canBuy,transCount,dp);
+        int buy,sell;
+        if(canBuy){
+
+buy = -prices[currentDay] + bestTime(prices, currentDay+1,0,transCount,dp);
+          dp[currentDay][canBuy][transCount] = max(idle,buy);
+        return dp[currentDay][canBuy][transCount];
+        }
+        else{
+            sell = prices[currentDay] + bestTime(prices,currentDay+1, 1, transCount-1,dp);
+           dp[currentDay][canBuy][transCount]  =max(idle,sell);
+            return  dp[currentDay][canBuy][transCount];
+        }
+         
+        
+    }
+    int maxProfit(vector<int>& prices) {
+       vector<vector<vector<int> > > dp (prices.size(),vector<vector<int> >(2,vector <int>(2,-1)));
+        return bestTime(prices,0,1,1,dp);
+    }
+};
