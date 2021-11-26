a5fd3055bac1bc879d2c6995e72f7344d5411fbe
Aryan Shaw <aryan4p130202@gmail.com>
2021-11-26 11:48:26 +0530

    Time: 230 ms (8.27%), Space: 106.1 MB (5.96%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,33 +1,40 @@
 class Solution {
 public:
-    int bestTime(vector<int>&prices,int currentDay,bool canBuy,int transCount,vector<vector<vector<int>>>&dp){
+    int dp[100001][2][2];
+    int bestTime(vector<int>&prices,int currentDay,bool canBuy,int transCount){
         if(currentDay >= prices.size() || transCount<=0){
             return 0;
         }
         
-        if(dp[currentDay][canBuy][transCount]!=-1)
-        {
-            return dp[currentDay][canBuy][transCount];
-        }
+       // string currentKey = to_string(currentDay) +"_"+ to_string(canBuy) +"_" + to_string(transCount);
         
-        int idle = bestTime(prices, currentDay+1, canBuy,transCount,dp);
-        int buy,sell;
-        if(canBuy){
-
-buy = -prices[currentDay] + bestTime(prices, currentDay+1,0,transCount,dp);
-          dp[currentDay][canBuy][transCount] = max(idle,buy);
-        return dp[currentDay][canBuy][transCount];
-        }
-        else{
-            sell = prices[currentDay] + bestTime(prices,currentDay+1, 1, transCount-1,dp);
-           dp[currentDay][canBuy][transCount]  =max(idle,sell);
+        
+        // if(dp.find(currentKey)!=dp.end()){
+        //     return dp[currentKey];
+        // }
+        
+        if( dp[currentDay][canBuy][transCount]!=-1){
             return  dp[currentDay][canBuy][transCount];
         }
-         
-        
+        int idle = bestTime(prices, currentDay+1, canBuy,transCount);
+        int buy=0,sell=0;
+        if(canBuy==1){
+
+buy = -prices[currentDay] + bestTime(prices, currentDay+1,0,transCount);
+     
+          buy=     max(idle,buy);
+        }
+        else{
+            sell = prices[currentDay] + bestTime(prices,currentDay+1, 1, transCount-1);
+            sell=max(idle,sell);
+           
+        }
+         dp[currentDay][canBuy][transCount] =  max(buy,sell);
+           return dp[currentDay][canBuy][transCount];         
     }
     int maxProfit(vector<int>& prices) {
-       vector<vector<vector<int> > > dp (prices.size(),vector<vector<int> >(2,vector <int>(2,-1)));
-        return bestTime(prices,0,1,1,dp);
+        // unordered_map<string,int>dp;
+        memset(dp,-1,sizeof(dp));
+        return bestTime(prices,0,1,1);
     }
 };
