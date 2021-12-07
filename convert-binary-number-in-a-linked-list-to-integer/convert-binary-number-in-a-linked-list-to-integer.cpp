395a1370401bf7ea0a832b7a04b0f20083415ce7
Aryan Shaw <aryan4p130202@gmail.com>
2021-12-07 12:41:43 +0530

    Time: 4 ms (46.25%), Space: 8.3 MB (16.65%) - LeetHub

--- convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.cpp
+++ convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -10,19 +10,6 @@
  */
 class Solution {
 public:
-    int convert(long long n) {
-  int dec = 0, i = 0, rem;
-
-  while (n!=0) {
-    rem = n % 10;
-    n /= 10;
-    dec += rem * pow(2, i);
-    ++i;
-  }
-
-  return dec;
-}
-    
     int getDecimalValue(ListNode* head) {
         // string current = "";
         int number=0;
