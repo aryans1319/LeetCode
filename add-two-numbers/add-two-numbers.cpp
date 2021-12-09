01eb8c0e563d1644a95b6f6d692eafe9f0ec64c6
Aryan Shaw <aryan4p130202@gmail.com>
2021-12-09 15:05:22 +0530

    Time: 24 ms (86.01%), Space: 71.4 MB (47.89%) - LeetHub

---
+++ add-two-numbers/add-two-numbers.cpp
@@ -0,0 +1,75 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *dummyNode = new ListNode(-1);
+      
+        ListNode *head = dummyNode;
+        if(l1 == NULL){
+            return l2;
+        }
+        if(l2==NULL){
+            return l1;
+        }
+
+        int carry = 0;
+        int total = 0;
+        while(l1&& l2){
+            int firstVal = l1->val;
+            int secondVal = l2->val;
+            
+            total = firstVal + secondVal + carry;
+            carry = total/10;
+            int res= total%10;
+             l1=l1->next;
+            l2=l2->next;
+                        
+            ListNode *newNode = new ListNode(res);
+            dummyNode->next = newNode;
+            
+            dummyNode=dummyNode->next;
+           
+        }
+        
+        while(l1){
+            int total = l1->val + carry;
+            
+        
+            carry = total/10;
+            int res = total % 10;
+             l1 = l1->next;
+            
+            // ListNode *newNode = new ListNode(res);
+            dummyNode->next = new ListNode(res);
+            dummyNode = dummyNode->next;
+           
+        }
+        while(l2){
+           int total = l2->val + carry;
+            
+           
+                carry = total/10;
+                int res = total %10;
+            l2 = l2->next;
+            
+            // ListNode *newNode = new ListNode(res);
+            dummyNode->next = new ListNode(res);
+            dummyNode = dummyNode->next;
+            
+        }
+        if(carry){
+            dummyNode->next = new ListNode(carry);
+            
+        }
+        return head->next;
+    }
+};
