5593acd9e1f3e7d07216341a76ed71583ed93884
Aryan Shaw <aryan4p130202@gmail.com>
2021-12-18 10:30:58 +0530

    Time: 47 ms (13.26%), Space: 25 MB (46.40%) - LeetHub

--- rotate-array/rotate-array.cpp
+++ rotate-array/rotate-array.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
-    void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int> copyArray(n);
-        for(int i=0;i<n;i++){
-            copyArray[(i+k)%n] = nums[i];
+    void reverse(vector<int> &nums, int start , int end){
+        while(start<=end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
-        nums = copyArray;
+    }
+    
+    void rotate(vector<int>& nums, int k) {
+        k = k%nums.size();
+        reverse(nums, 0,nums.size()-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.size()-1);
     }
 };
