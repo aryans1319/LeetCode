7ca889261e770a8e96f2ac83962f4a67e6ef0469
Aryan Shaw <aryan4p130202@gmail.com>
2021-12-23 00:52:55 +0530

    Time: 40 ms (57.74%), Space: 17.8 MB (76.71%) - LeetHub

---
+++ reorder-list/reorder-list.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+          // base case : linkedlist is empty
+        if (!head) return;
+        
+        // finding the middle with the help of two pointer approach
+        ListNode *tmp = head, *half = head, *prev = NULL;
+        while (tmp->next && tmp->next->next) {
+            tmp = tmp->next->next;
+            half = half->next;
+        }
+        
+        // adding one bit in case of lists with even length
+        if (tmp->next) half = half->next;
+        
+        // Now reverse the second half
+        while (half) {
+            tmp = half->next;
+            half->next = prev;
+            prev = half;
+            half = tmp;
+        }
+        half = prev;
+        
+        // After reversing the second half, let's merge both the halfes
+        while (head && half) {
+            tmp = head->next;
+            prev = half->next;
+            head->next = half;
+            half->next = tmp;
+            head = tmp;
+            half = prev;
+        }
+        
+        // Base case : closing when we had even length arrays
+        if (head && head->next) head->next->next = NULL;
+    }
+};
