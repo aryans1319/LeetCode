c4fd5896c0cf7f73cbe70b4ad1648dfc91407c1d
Aryan Shaw <aryan4p130202@gmail.com>
2021-12-30 14:53:59 +0530

    Time: 752 ms (5.05%), Space: 189.6 MB (12.91%) - LeetHub

---
+++ sliding-window-maximum/sliding-window-maximum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:  
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+          vector<int> output;
+        int i=0;
+        int j=0;
+        list<int> l;
+        while(j<nums.size())
+        {
+            if(l.empty()){ 
+                l.push_back(nums[j]);   
+            }
+            else
+            {
+                while(!l.empty() && l.back()<nums[j])
+                    l.pop_back();
+                l.push_back(nums[j]);               
+            }
+            if(j-i+1<k)
+                ++j;
+            else if(j-i+1==k)
+            {
+                output.push_back(l.front());
+                if(nums[i]==l.front())
+                    l.pop_front();
+                ++i;
+                ++j;
+            }
+        }
+        return output;
+    }
+};
