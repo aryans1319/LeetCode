25c2301064424acba4fc77ca62eccbcb14637093
Aryan Shaw <aryan4p130202@gmail.com>
2022-01-07 14:24:09 +0530

    Added solution - LeetHub

---
+++ 1's Complement - GFG/1s-complement.cpp
@@ -0,0 +1,33 @@
+// { Driver Code Starts
+ 
+#include<bits/stdc++.h>
+using namespace std; 
+
+ // } Driver Code Ends
+  
+class Solution{ 
+public:
+    string onesComplement(string S,int N){
+        //code here
+         for(int i =0;i<N;i++){
+            S[i] = S[i]^1;
+        }
+        return S;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        Solution ob;
+        cout<<ob.onesComplement(s,n)<<"\n";
+    }
+}  // } Driver Code Ends
