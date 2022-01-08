3d52d787bb0b5bb94cb47f30bfb9f6704ac05686
Aryan Shaw <aryan4p130202@gmail.com>
2022-01-08 16:36:50 +0530

    Time: 24 ms (43.86%), Space: 9 MB (34.18%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+         int i=0,j=0;
+    int mx = 0;
+    unordered_map<char,int> memo;
+    while(j<s.length()){
+        memo[s[j]]++;
+        
+        if(memo.size()>j-i+1){
+            j++;
+        }
+       
+         if(memo.size()<j-i+1){
+            while(memo.size()<j-i+1){
+                memo[s[i]]--;
+                if(memo[s[i]]==0)
+                {
+                    memo.erase(s[i]);
+                }
+                i++;
+            }
+            j++;
+        }
+        
+           if(memo.size()==j-i+1){
+            mx = max(mx,j-i+1);
+            j++;
+        }
+       
+        
+       
+    }
+    return mx;
+    }
+};
