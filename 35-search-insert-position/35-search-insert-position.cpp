874276140783825644e01651cf73cfa135113989
Aryan Shaw <aryan4p130202@gmail.com>
2022-01-08 16:33:20 +0530

    Time: 8 ms (24.47%), Space: 9.7 MB (72.10%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low = 0, high = nums.size()-1;
+
+        // Invariant: the desired index is between [low, high+1]
+        while (low <= high) {
+            int mid = low + (high-low)/2;
+
+            if (nums[mid] < target)
+                low = mid+1;
+            else
+                high = mid-1;
+        }
+
+        // (1) At this point, low > high. That is, low >= high+1
+        // (2) From the invariant, we know that the index is between [low, high+1], so low <= high+1. Follwing from (1), now we know low == high+1.
+        // (3) Following from (2), the index is between [low, high+1] = [low, low], which means that low is the desired index
+        //     Therefore, we return low as the answer. You can also return high+1 as the result, since low == high+1
+        return low;
+    }
+};
