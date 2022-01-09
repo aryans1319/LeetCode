dd994fae649484cb0c30b753817d441880324d50
Aryan Shaw <aryan4p130202@gmail.com>
2022-01-10 01:32:04 +0530

    Time: 3 ms (16.47%), Space: 6.3 MB (19.71%) - LeetHub

---
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isRobotBounded(string instructions) {
+         int x = 0, y = 0, i = 0;
+        vector<vector<int>> d = {{0, 1}, {1, 0}, {0, -1}, { -1, 0}};
+        for (char & ins : instructions)
+            if (ins == 'R')
+                i = (i + 1) % 4;
+            else if (ins == 'L')
+                i = (i + 3) % 4;
+            else
+                x += d[i][0], y += d[i][1];
+        return x == 0 && y == 0 || i > 0;
+    }
+};
