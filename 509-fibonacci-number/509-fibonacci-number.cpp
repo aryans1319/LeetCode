45f284c4ce340d1556f775ceffdf7fed2b55b2f6
Aryan Shaw <aryan4p130202@gmail.com>
2022-01-11 10:31:51 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (10.20%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int nthFibonacci(int n){
+    int nthFibonacci(int n,unordered_map<int,int>&mp){
         if(n==0){
             return 0;
         }
@@ -8,13 +8,22 @@ public:
             return 1;
         }
         
-        int leftCall = nthFibonacci(n-1);
-        int rightCall = nthFibonacci(n-2);
+        int currentKey = n;
         
-        return leftCall + rightCall;
+        if(mp.find(currentKey)!= mp.end()){
+            return mp[currentKey];
+        }
+        
+        int leftCall = nthFibonacci(n-1,mp);
+        int rightCall = nthFibonacci(n-2,mp);
+        
+        mp[currentKey] =  leftCall + rightCall;
+        
+        return mp[currentKey] ;
         
     }
     int fib(int n) {
-        return nthFibonacci(n);
+        unordered_map<int,int>mp;
+        return nthFibonacci(n,mp);
     }
 };
