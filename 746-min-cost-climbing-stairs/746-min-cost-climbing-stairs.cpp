1e90193f5f0a0d01a453ee438cc5bcc97d2cd847
Aryan Shaw <aryan4p130202@gmail.com>
2022-01-12 01:42:19 +0530

    Time: 16 ms (12.36%), Space: 15.5 MB (5.44%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
-    int minCost(vector<int>&cost, int currentIndex, unordered_map<int,int>&memo){
-        
+    int minCost(vector<int>&cost, int currentIndex,unordered_map<int,int>&mp){
         if(currentIndex == cost.size()){
             return 0;
         }
         
         if(currentIndex > cost.size()){
-            return 1001;
+            return 1000;
         }
         
         int currentKey = currentIndex;
         
-        if(memo.find(currentKey)!= memo.end()){
-            return memo[currentKey];
+        if(mp.find(currentKey)!=mp.end()){
+            return mp[currentKey];
         }
         
-        int oneJump = minCost(cost, currentIndex + 1,memo) + cost[currentIndex];
-        int twoJumps = minCost(cost, currentIndex + 2,memo) + cost[currentIndex];
+        int oneJump = minCost(cost, currentIndex + 1,mp) + cost[currentIndex];
+        int twoJump = minCost(cost, currentIndex + 2,mp) + cost[currentIndex];
         
-        memo[currentKey] = min(oneJump,twoJumps);
+        mp[currentKey] = min(oneJump,twoJump);
         
-        return memo[currentKey];
+        return  mp[currentKey];
+        
     }
     
-    
-    
     int minCostClimbingStairs(vector<int>& cost) {
-        unordered_map<int,int>memo;
-        int costFromIndex0 = minCost(cost,0,memo);
-        int costFromIndex1 = minCost(cost,1,memo);
-        
-        return min(costFromIndex0,costFromIndex1);
+        unordered_map<int,int>mp;
+        int a = minCost(cost,0,mp);
+        int b = minCost(cost,1,mp);
+        return min(a,b);
     }
 };
