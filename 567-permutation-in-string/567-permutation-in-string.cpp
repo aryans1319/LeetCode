d8647db73f00611680e68ca3e01e1a22a27d806e
Aryan Shaw <aryan4p130202@gmail.com>
2022-01-12 20:33:06 +0530

    Time: 12 ms (49.06%), Space: 7.2 MB (89.88%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        vector<int>s1hash(26,0);
+        vector<int>s2hash(26,0);
+        int s1len=s1.size();
+        int s2len=s2.size();
+        if(s1len>s2len)
+            return false;
+        
+        int left=0,right=0;
+        while(right<s1len){
+            s1hash[s1[right]-'a']+=1;
+            s2hash[s2[right]-'a']+=1;
+            right+=1;
+        }
+        right-=1;
+        while(right<s2len){
+            if(s1hash==s2hash)
+                return true;
+            right+=1;
+            if(right!=s2len)
+                s2hash[s2[right]-'a']+=1;
+            s2hash[s2[left]-'a']-=1;
+            left+=1;
+        }
+        return false;
+    }
+};
