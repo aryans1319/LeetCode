7e6406ba61354b9b747fdf6c0106b64ac25bf067
Aryan Shaw <aryan4p130202@gmail.com>
2022-01-17 01:51:06 +0530

    Time: 442 ms (10.14%), Space: 39.5 MB (21.59%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,46 +1,43 @@
 class Solution {
 public:
-    bool isPossible(vector<int>&nums, int currentIndex, int targetSum,unordered_map<string,bool>&mp)
-    {
-        if(targetSum == 0){
+    bool isPossibleToForm(vector<int>&nums,int currentIndex, int target,unordered_map<string,bool>&mp){
+        
+        if(target==0){
             return true;
         }
         
         if(currentIndex >= nums.size()){
             return false;
         }
-        
-        string currentKey = to_string(currentIndex) + "_" + to_string(targetSum);
+        string currentKey = to_string(currentIndex) + "_" + to_string(target);
         
         if(mp.find(currentKey)!=mp.end()){
             return mp[currentKey];
         }
         
-        bool consider = false;
-        if(nums[currentIndex] <= targetSum){
-        consider = isPossible(nums, currentIndex + 1, targetSum - nums[currentIndex],mp);
+        bool pick = false;
+        if(nums[currentIndex] <= target){
+            pick = isPossibleToForm(nums, currentIndex+1,target-nums[currentIndex],mp);
         }
-        if(consider){
+        if(pick){
             mp[currentKey] = true;
-            return mp[currentKey]; 
+            return mp[currentKey];
         }
+        bool notPick = isPossibleToForm(nums,currentIndex+1,target,mp);
+        mp[currentKey] = pick || notPick;
+        return mp[currentKey];
         
-        bool notConsider = isPossible(nums,currentIndex+1,targetSum,mp);
-        mp[currentKey] = consider || notConsider;
-        return      mp[currentKey];
     }
-    
-    
-    
     bool canPartition(vector<int>& nums) {
-        int targetSum = 0;
-        for(auto i : nums){
-            targetSum+=i;
+        int sum = 0;
+        for(auto i: nums){
+            sum+=i;
         }
-        if(targetSum%2!=0){
+        if(sum%2!=0){
             return false;
         }
+        int target = sum/2;
         unordered_map<string,bool>mp;
-        return isPossible(nums,0,targetSum/2,mp);
+        return isPossibleToForm(nums,0,target,mp);
     }
 };
