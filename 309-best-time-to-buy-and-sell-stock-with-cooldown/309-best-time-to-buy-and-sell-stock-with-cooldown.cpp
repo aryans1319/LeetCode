ea9e0951dc36aa3bdaa68d85c93de235f06eff4f
Aryan Shaw <aryan4p130202@gmail.com>
2022-01-22 02:16:48 +0530

    Time: 8 ms (37.08%), Space: 11.4 MB (28.89%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,39 +1,28 @@
 class Solution {
 public:
-     int dp[5001][2];
-    int bestTime(vector<int>&prices,int currentDay,bool canBuy){
+    int dp[5001][2];
+    int bestTime(vector<int>&prices, int currentDay, bool canBuy){
         if(currentDay >= prices.size()){
             return 0;
         }
         
-       // string currentKey = to_string(currentDay) +"_"+ to_string(canBuy) +"_" + to_string(transCount);
-        
-        
-        // if(dp.find(currentKey)!=dp.end()){
-        //     return dp[currentKey];
-        // }
-        
-        if( dp[currentDay][canBuy]!=-1){
+        if(dp[currentDay][canBuy]!=-1){
+            return dp[currentDay][canBuy];
+        }
+        int idle = bestTime(prices,currentDay+1,canBuy);
+        int buy=0,sell=0;
+        if(canBuy){
+             buy = -prices[currentDay] + bestTime(prices,currentDay+1,0);
+            dp[currentDay][canBuy] =  max(buy,idle);
             return  dp[currentDay][canBuy];
         }
-        int idle = bestTime(prices, currentDay+1, canBuy);
-        int buy=0,sell=0;
-        if(canBuy==1){
-
-buy = -prices[currentDay] + bestTime(prices, currentDay+1,0);
-     
-          buy=     max(idle,buy);
-        }
         else{
-            sell = prices[currentDay] + bestTime(prices,currentDay+2, 1);
-            sell=max(idle,sell);
-           
+            sell = prices[currentDay] + bestTime(prices,currentDay+2,1);
+            dp[currentDay][canBuy] =  max(sell,idle);
+            return  dp[currentDay][canBuy];
         }
-         dp[currentDay][canBuy] =  max(buy,sell);
-           return dp[currentDay][canBuy];         
     }
     int maxProfit(vector<int>& prices) {
-        // unordered_map<string,int>dp;
         memset(dp,-1,sizeof(dp));
         return bestTime(prices,0,1);
     }
