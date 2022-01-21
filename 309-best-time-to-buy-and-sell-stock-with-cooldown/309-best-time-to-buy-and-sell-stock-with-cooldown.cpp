473941995e5fbe3dca05123613578d2d2482ea5c
Aryan Shaw <aryan4p130202@gmail.com>
2022-01-21 19:35:28 +0530

    Time: 4 ms (71.55%), Space: 11.7 MB (23.99%) - LeetHub

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+     int dp[5001][2];
+    int bestTime(vector<int>&prices,int currentDay,bool canBuy){
+        if(currentDay >= prices.size()){
+            return 0;
+        }
+        
+       // string currentKey = to_string(currentDay) +"_"+ to_string(canBuy) +"_" + to_string(transCount);
+        
+        
+        // if(dp.find(currentKey)!=dp.end()){
+        //     return dp[currentKey];
+        // }
+        
+        if( dp[currentDay][canBuy]!=-1){
+            return  dp[currentDay][canBuy];
+        }
+        int idle = bestTime(prices, currentDay+1, canBuy);
+        int buy=0,sell=0;
+        if(canBuy==1){
+
+buy = -prices[currentDay] + bestTime(prices, currentDay+1,0);
+     
+          buy=     max(idle,buy);
+        }
+        else{
+            sell = prices[currentDay] + bestTime(prices,currentDay+2, 1);
+            sell=max(idle,sell);
+           
+        }
+         dp[currentDay][canBuy] =  max(buy,sell);
+           return dp[currentDay][canBuy];         
+    }
+    int maxProfit(vector<int>& prices) {
+        // unordered_map<string,int>dp;
+        memset(dp,-1,sizeof(dp));
+        return bestTime(prices,0,1);
+    }
+};
