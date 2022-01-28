ff43f93f1987b1a60faf868d08a5173682f404b1
Aryan Shaw <aryan4p130202@gmail.com>
2022-01-29 00:29:27 +0530

    Time: 3 ms (50.18%), Space: 6.6 MB (91.91%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int totalWays(vector<int>&nums, int currentIndex,int target,vector<int>&mp){
+    int mp[1001];
+    int totalWays(vector<int>&nums, int currentIndex,int target){
         if(currentIndex >= nums.size()|| target<=0){
             return (target==0) ? 1:0;
         }
@@ -8,13 +9,13 @@ public:
         if(mp[target]!=-1){
             return mp[target];
         }
-        int pick =  totalWays(nums,0, target - nums[currentIndex],mp);
-        int notPick = totalWays(nums, currentIndex+1, target,mp);
+        int pick =  totalWays(nums,0, target - nums[currentIndex]);
+        int notPick = totalWays(nums, currentIndex+1, target);
         mp[target] = pick+notPick;
         return mp[target];
     }
     int combinationSum4(vector<int>& nums, int target) {
-        vector<int>mp(target+1,-1);
-        return totalWays(nums,0,target,mp);
+        memset(mp,-1,sizeof(mp));
+        return totalWays(nums,0,target);
     }
 };
