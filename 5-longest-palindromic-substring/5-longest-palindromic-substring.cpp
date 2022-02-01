f419e870a1361165ff88dccc7f0c2e327a2a0df6
Aryan Shaw <aryan4p130202@gmail.com>
2022-02-02 01:30:16 +0530

    Time: 20 ms (85.11%), Space: 7.1 MB (92.74%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if(s.length()<=1) return s;
+        
+        int max_len=1;
+        int n=s.length();
+        int st=0,end=0;
+        
+        //For odd length
+        for(int i=0;i<n;i++){
+            int l=i,r=i;
+            while(l>=0 && r<n){
+                if(s[l]==s[r]){
+                    l--;r++;
+                }else{
+                    break;
+            }
+            }
+            int len=r-l-1;
+            if(len>max_len){
+                max_len=len;
+            st=l+1;
+            end=r-1;
+        }}
+        
+           //For even length
+        for(int i=0;i<n;i++){
+            int l=i,r=i+1;
+            while(l>=0 && r<n){
+                if(s[l]==s[r]){
+                    l--;r++;
+                }
+                else{
+                    break;}
+            }
+            int len=r-l-1;
+            if(len>max_len){
+                max_len=len;
+            st=l+1;
+            end=r-1;
+        }
+        }
+        
+        return s.substr(st,max_len);
+        
+    }
+};
