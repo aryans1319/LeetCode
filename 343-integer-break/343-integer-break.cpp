e4b9f45cd60acff8261304a6ad34011290d35f3d
Aryan Shaw <aryan4p130202@gmail.com>
2022-02-01 09:43:48 +0530

    Time: 5 ms (11.58%), Space: 6 MB (72.32%) - LeetHub

---
+++ 343-integer-break/343-integer-break.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+          if(n == 2) return 1;
+        if(n == 3) return 2;
+        if(n == 4) return 4;
+        
+        int product = 1;
+        while(n > 4){
+            product *= 3;
+            n -= 3;
+        }
+        product *= n;
+        
+        return product;
+    }
+};
