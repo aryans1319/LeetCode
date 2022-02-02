e3cbd3105e687a5c58cecdd4f06a627d5c7a9f1a
Aryan Shaw <aryan4p130202@gmail.com>
2022-02-02 19:40:35 +0530

    Time: 49 ms (18.93%), Space: 12 MB (55.48%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     void sourceToTarget(vector<vector<int>>&graph, int currentNode,vector<bool>visited,int n , vector<int>&currentPath,vector<vector<int>>&answer){
         if(currentNode == n-1){
-            currentPath.push_back(currentNode);
+            // currentPath.push_back(currentNode);
             answer.push_back(currentPath);
-            currentPath.pop_back();
+            // currentPath.pop_back();
             return;
         }
         
@@ -13,14 +13,16 @@ public:
         }
         
         visited[currentNode] = true;
-        currentPath.push_back(currentNode);
+        // currentPath.push_back(currentNode);
         
         for(auto neighbour : graph[currentNode]){
+            currentPath.push_back(neighbour);
             sourceToTarget(graph,neighbour,visited,n,currentPath,answer);
+            currentPath.pop_back();
         }
         
         visited[currentNode] = false;
-        currentPath.pop_back();
+        // currentPath.pop_back();
         return;
     }
     
@@ -28,6 +30,7 @@ public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         vector<vector<int>>answer;
         vector<int>currentPath;
+        currentPath.push_back(0);
         int n = graph.size();
         vector<bool> vis(n);
         sourceToTarget(graph,0,vis,n,currentPath,answer);
