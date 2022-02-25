dce3cb9c9af9f3ce502521de0a59401ae00869ad
Aryan Shaw <aryan4p130202@gmail.com>
2022-02-26 02:00:54 +0530

    Time: 40 ms (79.01%), Space: 32.2 MB (87.47%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
@@ -12,45 +12,16 @@
 class Solution {
 public:
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-        if(root1 == NULL) return root2;
-        if(root2 == NULL) return root1;
-        if(root1==NULL && root2 ==NULL) return NULL;
+        if(root1==NULL) return root2;
+        if(root2== NULL) return root1;
+        if(root1!=NULL && root2!=NULL){
+        root1->val+=root2->val;
+            
+            root1->left = mergeTrees(root1->left,root2->left);
+            root1->right = mergeTrees(root1->right,root2->right);
         
-        queue<pair<TreeNode*,TreeNode*>>q;
-        q.push({root1,root2});
-        
-        while(!q.empty()){
-            auto temp = q.front();
-            q.pop();
-            
-            /* if the right subtree is not null or the root of the right sub-tree (either left or right isnt null)
-               then we will process further */ 
-            
-            if(temp.second!=NULL){
-                temp.first->val += temp.second->val;
-            
-            
-            /* if the left node of the first subtree is null we can just point the left node of second tree to the
-               left node of the first subtree */ 
-            
-            if(temp.first->left == NULL){
-                temp.first->left = temp.second->left;
-            }
-            else{
-                q.push({temp.first->left,temp.second->left});
-            }
-            
-             /* if the right node of the first subtree is null we can just point the right node of second tree to the
-               right node of the first subtree */ 
-            
-            if(temp.first->right == NULL){
-                temp.first->right = temp.second->right;
-            }
-            else{
-                q.push({temp.first->right,temp.second->right});
-            }
-            }
         }
+        
         return root1;
     }
 };
