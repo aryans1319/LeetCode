800150bb346d3e49114f90d9f1426ce87724f493
Aryan Shaw <aryan4p130202@gmail.com>
2022-03-05 19:14:40 +0530

    Time: 32 ms (5.81%), Space: 152.4 MB (12.60%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
-    int numberOfArithmeticSlices(vector<int>& A) {
-       int n = A.size();
-        if (n < 3) return 0;
-        vector<int> dp(n, 0); // dp[i] means the number of arithmetic slices ending with A[i]
-        if (A[2]-A[1] == A[1]-A[0]) dp[2] = 1; // if the first three numbers are arithmetic or not
-        int result = dp[2];
-        for (int i = 3; i < n; ++i) {
-            // if A[i-2], A[i-1], A[i] are arithmetic, then the number of arithmetic slices ending with A[i] (dp[i])
-            // equals to:
-            //      the number of arithmetic slices ending with A[i-1] (dp[i-1], all these arithmetic slices appending A[i] are also arithmetic)
-            //      +
-            //      A[i-2], A[i-1], A[i] (a brand new arithmetic slice)
-            // it is how dp[i] = dp[i-1] + 1 comes
-            if (A[i]-A[i-1] == A[i-1]-A[i-2]) 
-                dp[i] = dp[i-1] + 1;
-            result += dp[i]; // accumulate all valid slices
-        }
-        return result; 
+     
+    int memo(vector<int> nums, int n, int s,unordered_map<int,int>&mp){
+        if(n < 3) return 0;
+        if(mp.find(n) != mp.end()) 
+            return mp[n];
+        if(nums[n-3]-nums[n-2] == nums[n-2]-nums[n-1]) 
+            return mp[n] = s+memo(nums, n-1, s+1,mp);
+        else 
+            return mp[n] = memo(nums, n-1, 1,mp);
+    }
+    
+    int numberOfArithmeticSlices(vector<int>& nums) {
+          unordered_map<int,int>mp;
+          return memo(nums, nums.size(), 1, mp);
     }
 };
