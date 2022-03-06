46e9fbef70d10d0d76a7daef6140f8f1ba78293f
Aryan Shaw <aryan4p130202@gmail.com>
2022-03-06 14:03:47 +0530

    Time: 4 ms (45.97%), Space: 9.7 MB (10.88%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
      
-    int memo(vector<int> nums, int n, int s,unordered_map<int,int>&mp){
-        if(n < 3) return 0;
-        if(mp.find(n) != mp.end()) 
+         int fun (vector <int> &a, int n, int counter,unordered_map<int,int>&mp)
+    {
+        if (n < 3) return 0;
+        if(mp.find(n)!=mp.end()){
             return mp[n];
-        if(nums[n-3]-nums[n-2] == nums[n-2]-nums[n-1]) 
-            return mp[n] = s+memo(nums, n-1, s+1,mp);
-        else 
-            return mp[n] = memo(nums, n-1, 1,mp);
+        }     
+             
+        if (a[n-1] + a[n-3] == 2 * a[n-2]) { 
+            counter ++; 
+            return mp[n] = counter + fun (a, n-1, counter,mp);
+        }
+        else{
+            return mp[n] = fun (a, n-1, 0,mp);
+        }
     }
+    
     
     int numberOfArithmeticSlices(vector<int>& nums) {
           unordered_map<int,int>mp;
-          return memo(nums, nums.size(), 1, mp);
-    }
+          return fun(nums, nums.size(), 0, mp);
+}
 };
