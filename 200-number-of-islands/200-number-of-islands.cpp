e011601e77ad49d28d10810556f165f68c423dd5
Aryan Shaw <aryan4p130202@gmail.com>
2022-03-14 18:57:48 +0530

    Time: 61 ms (32.86%), Space: 20.2 MB (9.47%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    void bfs(vector<vector<char>>& grid,int currentRow,int currentCol,int m,int n){
+    void countIslands(vector<vector<char>>& grid,int currentRow,int currentCol,int m,int n){
         queue<pair<int,int>>q;
         q.push({currentRow,currentCol});
         
         while(!q.empty()){
             auto it = q.front();
             q.pop();
-            int row = it.first;
-            int col = it.second;
-            int dx [] = {1,-1,0,0};
-            int dy [] = {0,0,-1,1};
-            
-            for(int currentDir = 0;currentDir<4;currentDir++){
-                int curr_row = row + dx[currentDir];
-                int curr_col = col + dy[currentDir];
-                if(curr_row < 0 || curr_row>=m ||curr_col < 0 || curr_col >=n || grid[curr_row][curr_col] == '0'){
-                    continue;
-                }
-                q.push({curr_row,curr_col});
-                grid[curr_row][curr_col] = '0';
-
+            int curr_row = it.first;
+            int curr_col = it.second;
+            if(curr_row < 0 || curr_row>=m ||curr_col < 0 || curr_col >=n || grid[curr_row][curr_col] == '0'){
+            continue;
             }
+            grid[curr_row][curr_col] = '0';
+            q.push({curr_row-1,curr_col});
+            q.push({curr_row,curr_col+1});
+            q.push({curr_row+1,curr_col});
+            q.push({curr_row,curr_col-1});
         }
+        return;
     }
     
     int numIslands(vector<vector<char>>& grid) {
@@ -34,7 +30,7 @@ public:
             for(int currentCol = 0;currentCol < n;currentCol++){
                 if(grid[currentRow][currentCol] == '1'){
                     ans+=1;
-                    bfs(grid,currentRow,currentCol,m,n);
+                    countIslands(grid,currentRow,currentCol,m,n);
                 }
             }
         }
