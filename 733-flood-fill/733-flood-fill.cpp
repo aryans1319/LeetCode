f5725aee8a4266be64287ca9b84bc6c338763fb3
Aryan Shaw <aryan4p130202@gmail.com>
2022-03-14 13:39:17 +0530

    Time: 20 ms (20.62%), Space: 14.1 MB (50.91%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>&grid, int sr, int sc, int newColor) {
-        if(grid[sr][sc]==newColor) return grid;
-        int m = grid.size();
-        int n = grid[0].size();
-        int color = grid[sr][sc];
-        queue<pair<int,int>>q;
-        q.push({sr,sc});
-        while(!q.empty())
-        {
-            auto it=q.front();
-            q.pop();
-            int curr_row = it.first;
-            int curr_col = it.second;
-            if(curr_row<0 || curr_row>=m || curr_col<0 || curr_col>=n || grid[curr_row][curr_col]!=color)
-                continue;
-            grid[curr_row][curr_col] = newColor;
-            q.push({curr_row-1,curr_col});
-             q.push({curr_row,curr_col+1});
-             q.push({curr_row+1,curr_col});
-             q.push({curr_row,curr_col-1});
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        if(image[sr][sc] == newColor){
+            return image;
         }
-        return grid;
+        dfs(image,sr,sc,image[sr][sc],image.size(),image[0].size(),newColor);
+        return image;
+    }
+    
+    void dfs(vector<vector<int>>& image,int currentRow,int currentCol,int color,int m,int n,int newColor){
+        if(currentRow < 0 || currentRow >=m || currentCol < 0 || currentCol >=n || image[currentRow][currentCol]!=color){
+            return;
+        }
+        image[currentRow][currentCol] = newColor;
+        
+        // up move
+        dfs(image,currentRow - 1,currentCol,color,m,n,newColor);
+        
+        // down move
+        dfs(image,currentRow + 1,currentCol,color,m,n,newColor);
+        
+        //right move
+        dfs(image,currentRow,currentCol+1,color,m,n,newColor);
+        
+        //left move
+        dfs(image,currentRow,currentCol-1,color,m,n,newColor);
+        
+        return;
     }
 };
