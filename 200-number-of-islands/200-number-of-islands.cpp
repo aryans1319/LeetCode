8463313e484a1159d801970999911050b3014327
Aryan Shaw <aryan4p130202@gmail.com>
2022-03-14 14:35:47 +0530

    Time: 68 ms (25.43%), Space: 12.2 MB (84.35%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
+    void countIslands(vector<vector<char>>& grid,int currentRow,int currentCol,int m,int n){
+        if(currentRow < 0 || currentRow>=m || currentCol < 0 || currentCol >=n || grid[currentRow][currentCol] == '0')
+            return;
+        
+        grid[currentRow][currentCol] = '0';
+        countIslands(grid,currentRow-1,currentCol,m,n);
+        countIslands(grid,currentRow+1,currentCol,m,n);
+        countIslands(grid,currentRow,currentCol-1,m,n);
+        countIslands(grid,currentRow,currentCol+1,m,n);
+        
+        return;
+    }
+    
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        int ans =0;
-        for(int currentRow = 0;currentRow <m;currentRow++){
-            for(int currentCol =0;currentCol < n;currentCol++){
+        
+        int ans = 0;
+        for(int currentRow = 0;currentRow < m;currentRow++){
+            for(int currentCol = 0;currentCol < n;currentCol++){
                 if(grid[currentRow][currentCol] == '1'){
                     ans+=1;
-                    dfs(grid,currentRow,currentCol,m,n);
+                    countIslands(grid,currentRow,currentCol,m,n);
                 }
             }
         }
         return ans;
-    }
-    
-    void dfs(vector<vector<char>>& grid,int currentRow,int currentCol,int m,int n){
-         if(currentRow < 0 || currentRow >=m || currentCol < 0 || currentCol >=n || grid[currentRow][currentCol] == '0'){
-            return;
-        }
-        grid[currentRow][currentCol] = '0';
-        
-        dfs(grid,currentRow-1,currentCol,m,n);
-         dfs(grid,currentRow+1,currentCol,m,n);
-         dfs(grid,currentRow,currentCol-1,m,n);
-         dfs(grid,currentRow,currentCol+1,m,n);
-        
-        return;
     }
 };
