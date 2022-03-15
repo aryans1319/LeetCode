bc6b52d20829609a1e6f37cac99586fa336bfeef
Aryan Shaw <aryan4p130202@gmail.com>
2022-03-15 11:05:40 +0530

    Time: 25 ms (60.27%), Space: 23.2 MB (89.09%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
-    int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int ans = 0;
-        n = grid.size(), m = grid[0].size();
-        for (int i = 0; i < n; i++) 
-            for (int j = 0; j < m; j++)
-                if (grid[i][j]) ans = max(ans, trav(i, j, grid));
-        return ans;
+     int countIslands(vector<vector<int>>& grid,int currentRow,int currentCol,int m,int n){
+        if(currentRow < 0 || currentRow>=m || currentCol < 0 || currentCol >=n || grid[currentRow][currentCol] == 0)
+            return 0;
+        
+        grid[currentRow][currentCol] = 0;
+        int a = countIslands(grid,currentRow-1,currentCol,m,n);
+        int b =countIslands(grid,currentRow+1,currentCol,m,n);
+        int c =countIslands(grid,currentRow,currentCol-1,m,n);
+        int d =countIslands(grid,currentRow,currentCol+1,m,n);
+        
+        return 1 + a+b+c+d;
     }
-private:
-    int n, m;
-    int trav(int i, int j, vector<vector<int>>& grid) {
-        if (i < 0 || j < 0 || i >= n || j >= m || !grid[i][j]) return 0;
-        grid[i][j] = 0;
-        return 1 + trav(i-1, j, grid) + trav(i, j-1, grid) + trav(i+1, j, grid) + trav(i, j+1, grid);
+    
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        int ans = 0;
+        for(int currentRow = 0;currentRow < m;currentRow++){
+            for(int currentCol = 0;currentCol < n;currentCol++){
+                if(grid[currentRow][currentCol] == 1){
+                    // ans+=1;
+                    ans = max(ans,countIslands(grid,currentRow,currentCol,m,n));
+                }
+            }
+        }
+        return ans;
     }
 };
