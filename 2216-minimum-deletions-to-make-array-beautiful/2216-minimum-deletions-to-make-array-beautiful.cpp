ca087b6d7aa126e749e027c2eb1a1cc9ac9b0c23
Aryan Shaw <aryan4p130202@gmail.com>
2022-03-27 15:32:20 +0530

    Time: 233 ms (14.29%), Space: 121.1 MB (57.14%) - LeetHub

--- 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.cpp
+++ 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
     int minDeletion(vector<int>& nums) {
-        // int n=nums.size();
-        // int shift = 0;
-        // for(int i=0;i<n-1;i++){
-        //     if((i+shift)%2 == 0){
-        //         if(nums[i] == nums[i+1]){
-        //             shift++;
-        //         }
-        //     }
-        // }
-        // if(n - shift %2 ==1){
-        //     shift++;
-        // }
-        // return shift;
+        int n=nums.size();
         int shift = 0;
-            for (int i = 0; i < nums.size() - 1; i++)
-            {
-                if ((i + shift) % 2 == 0)
-                {
-                    if (nums[i] == nums[i + 1])
-                    {
-                        shift++;
-                    }
+        for(int i=0;i<n-1;i++){
+            if((i+shift)%2 == 0){
+                if(nums[i] == nums[i+1]){
+                    shift++;
                 }
             }
-            if ((nums.size() - shift) % 2 == 1)
-            {
-                shift++;
-            }
-            return shift;
+        }
+        if((n - shift) %2 ==1){
+            shift++;
+        }
+        return shift;
+        // int shift = 0;
+        //     for (int i = 0; i < nums.size() - 1; i++)
+        //     {
+        //         if ((i + shift) % 2 == 0)
+        //         {
+        //             if (nums[i] == nums[i + 1])
+        //             {
+        //                 shift++;
+        //             }
+        //         }
+        //     }
+        //     if ((nums.size() - shift) % 2 == 1)
+        //     {
+        //         shift++;
+        //     }
+        //     return shift;
     }
 };
