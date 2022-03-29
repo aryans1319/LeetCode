9dcf0be40d211b99f729c94e51d55cdf292a4eb0
Aryan Shaw <aryan4p130202@gmail.com>
2022-03-29 20:29:12 +0530

    Time: 4 ms (37.94%), Space: 7.8 MB (83.79%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
-       int maxMoneyLooted(vector<int>&nums,int n, int currentIndex,unordered_map<int,int>&mp){
-        
-        //base condition
-        
-        if(currentIndex >= n){
-            return 0;
+    long long int robb(vector<long long int>& nums) {
+        long long int n = nums.size();
+        long long int prev1 = nums[0];
+       long long int prev2 = 0;
+        for(long long int i=1;i<n;i++){
+            long long int pick = nums[i];
+            if(i>1){
+                pick+=prev2;
+            }
+            long long int notPick = 0 + prev1;
+            long long int curi = max(pick,notPick);
+            prev2 = prev1;
+            prev1 = curi;
         }
-        //choices
-        int currentKey = currentIndex;
-        
-        if(mp.find(currentKey)!=mp.end()){
-            return mp[currentKey];
-        }
-        
-        int lootenge = maxMoneyLooted(nums,n,currentIndex+2,mp) + nums[currentIndex];
-        int nhiLootenge = maxMoneyLooted(nums,n,currentIndex+1,mp);
-        
-        mp[currentKey] = max(lootenge,nhiLootenge);
-        
-        return   mp[currentKey];
-        
+        return prev1;
     }
+    int rob(vector<int>& valueInHouse) {
+       vector<long long int>first,second;
+    long long int n = valueInHouse.size();
+    if(n==1) return valueInHouse[0];
     
-    int rob(vector<int>& nums) {
-        // vector<int>temp1,temp2;
-        int n = nums.size();
-        if(n==1)return nums[0];
-        unordered_map<int,int>mp,mp1;
-        int first = maxMoneyLooted(nums,n-1,0,mp);
-        int second = maxMoneyLooted(nums,n,1,mp1);
-        // cout<<first<<" ";
-        // cout<<second;
-        return max(first,second);
-        
+    for(long long int i=0;i<n;i++){
+        if(i!=0) first.push_back(valueInHouse[i]);
+        if(i!=n-1) second.push_back(valueInHouse[i]);
     }
+     long long int ans1 = robb(first);
+     long long int ans2 = robb(second);
+    return max(ans1,ans2);
+    }
+
 };
