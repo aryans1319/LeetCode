2d5dab49e16ebc67151624bb29a927d111033d3e
Aryan Shaw <aryan4p130202@gmail.com>
2022-03-29 20:09:54 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (76.02%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
-    int maxMoneyLooted(vector<int>&nums, int currentIndex,unordered_map<int,int>&mp){
-        
-        //base condition
-        
-        if(currentIndex >= nums.size()){
-            return 0;
-        }
-        //choices
-        int currentKey = currentIndex;
-        
-        if(mp.find(currentKey)!=mp.end()){
-            return mp[currentKey];
-        }
-        
-        int lootenge = maxMoneyLooted(nums,currentIndex+2,mp) + nums[currentIndex];
-        int nhiLootenge = maxMoneyLooted(nums,currentIndex+1,mp);
-        
-        mp[currentKey] = max(lootenge,nhiLootenge);
-        
-        return   mp[currentKey];
-        
-    }
-    
     int rob(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        return maxMoneyLooted(nums,0,mp);
+        int n = nums.size();
+        int prev1 = nums[0];
+        int prev2 = 0;
+        for(int i=1;i<n;i++){
+            int pick = nums[i];
+            if(i>1){
+                pick+=prev2;
+            }
+            int notPick = 0 + prev1;
+            int curi = max(pick,notPick);
+            prev2 = prev1;
+            prev1 = curi;
+        }
+        return prev1;
     }
 };
