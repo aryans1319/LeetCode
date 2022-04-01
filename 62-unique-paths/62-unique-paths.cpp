9009c3be5ef56d0f32e9c13c62832b30e3a320be
Aryan Shaw <aryan4p130202@gmail.com>
2022-04-01 10:56:33 +0530

    Time: 4 ms (24.11%), Space: 6.5 MB (43.68%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -15,7 +15,7 @@ public:
         return dp[i][j]=up+left;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
+        vector<vector<int>>dp(m,vector<int>(n,0));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(i==0 && j==0) dp[i][j] = 1;
