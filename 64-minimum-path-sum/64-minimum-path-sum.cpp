81575d6b221692e05fb766d1b8e418ead3e4bd14
Aryan Shaw <aryan4p130202@gmail.com>
2022-04-01 12:52:36 +0530

    Time: 12 ms (59.62%), Space: 10.2 MB (6.00%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
-    
-    int getMinSum(vector<vector<int>> &grid, int currentIndexofI ,int currentIndexofJ, vector<vector<int>>&memo){        
-        if(currentIndexofI > grid.size()-1 || currentIndexofJ > grid[0].size()-1)
-        {
-            return 99999;
+    int f(int i,int j,vector<vector<int>>&a,vector<vector<int>>&dp){
+        if(i==0 && j==0) return a[0][0];
+        if(i < 0 || j < 0) return 99999;
+        
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
         
+        int up = a[i][j] + f(i-1,j,a,dp);
+        int down = a[i][j] + f(i,j-1,a,dp);
         
-        if(currentIndexofI == grid.size()-1 && currentIndexofJ == grid[0].size()-1 ){
-            return grid[currentIndexofI][currentIndexofJ];
-        }
-        
-        if(memo[currentIndexofI][currentIndexofJ]!=-1)
-            return memo[currentIndexofI][currentIndexofJ];
-        
-        
-            int moveRight = getMinSum(grid, currentIndexofI , currentIndexofJ+1,memo )  + grid[currentIndexofI][currentIndexofJ];
-            int moveDown = getMinSum(grid, currentIndexofI+1,currentIndexofJ,memo  )  + grid[currentIndexofI][currentIndexofJ];
-            
-            memo[currentIndexofI][currentIndexofJ] = min(moveDown,moveRight)  ;
-                
-             return memo[currentIndexofI][currentIndexofJ];
+        return dp[i][j] = min(up,down);
         
     }
-    
     int minPathSum(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-         vector<vector<int>> memo(m,vector<int>(n, -1));
-        return getMinSum(grid,0,0,memo);
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+
+        return f(m-1,n-1,grid,dp);
     }
 };
