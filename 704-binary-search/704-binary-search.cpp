469480b7e6483ba7ea3e9156f252991827717e5f
Aryan Shaw <aryan4p130202@gmail.com>
2022-04-06 03:10:10 +0530

    Time: 54 ms (42.28%), Space: 27.5 MB (92.46%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -10,11 +10,11 @@ public:
             if(nums[mid] == target){
                 return mid;
             }
-            else if(nums[mid] < target){
-                start = mid+1;
+            else if(nums[mid] > target){
+                end = mid-1;
             }
             else{
-                end = mid-1;
+                start = mid+1;
             }
         }
         return -1;
