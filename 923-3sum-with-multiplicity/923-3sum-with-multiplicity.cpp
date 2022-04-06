60b0263a0bf9e66cf50af743592c6400bd44faa4
Aryan Shaw <aryan4p130202@gmail.com>
2022-04-07 01:25:54 +0530

    Time: 493 ms (42.96%), Space: 10.5 MB (74.57%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -1,14 +1,36 @@
 class Solution {
 public:
-    int threeSumMulti(vector<int>& arr, int X) {
-          int n = arr.size(), mod = 1e9+7, ans = 0;
-        unordered_map<int, int> m;
+    int threeSumMulti(vector<int>& arr, int target) {
+          int n=arr.size();
+      sort(arr.begin(),arr.end());
         
-        for(int i=0; i<n; i++) {
-            ans = (ans + m[X - arr[i]]) % mod;
-            
-            for(int j=0; j<i; j++) m[arr[i] + arr[j]]++;
-        }
-        return ans;
+      int ans=0;
+      int mod=1e9+7;
+        
+      for(int i=0;i<n-2;i++){
+          
+          int lo=i+1,hi=n-1;
+          
+          while(lo<hi){
+              if(arr[i]+arr[lo]+arr[hi]==target){
+                  int cntlo=1,cnthi=1;
+                  while (lo < hi && arr[lo] == arr[lo+1]) {cntlo++; lo++;}
+                    while (lo < hi && arr[hi] == arr[hi-1]) {cnthi++; hi--;}
+                    if (lo == hi) 
+                    ans =(ans+cntlo*(cntlo-1)/2)%mod;
+                    else 
+                    ans = (ans+cntlo*cnthi)%mod;
+                    lo++; hi--; 
+              }
+              else if(arr[i]+arr[lo]+arr[hi]>target){
+                  hi--;
+              }
+              else{
+                  lo++;
+              }
+          }
+          
+      }  
+     return ans;    
     }
 };
