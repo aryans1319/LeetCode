3be6f0ae6802fd99ddc51f8c7120a0a38881251a
Aryan Shaw <aryan4p130202@gmail.com>
2022-04-10 20:00:00 +0530

    Added solution - LeetHub

--- Largest subarray with 0 sum - GFG/largest-subarray-with-0-sum.cpp
+++ Largest subarray with 0 sum - GFG/largest-subarray-with-0-sum.cpp
@@ -16,16 +16,21 @@ class Solution{
         int ans = 0;
         
         unordered_map<int,int>mp;
-        mp[0] = -1;
+        // mp[0] = -1;
         for(int i=0;i<n;i++){
             prefixSum+=A[i];
             
+            if(prefixSum == 0){
+                ans = i+1;
+            }
+            else{
             if(mp.find(prefixSum)!=mp.end()){
                 int lastOccIndexLen = i - mp[prefixSum];
                 ans = max(ans,lastOccIndexLen);
             }
             else{
                 mp[prefixSum] = i;
+            }
             }
         }
         return ans;
