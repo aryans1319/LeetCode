55ee1de5adea0d0d70a4ed3af65bd9d000ae4492
Aryan Shaw <aryan4p130202@gmail.com>
2022-04-10 15:24:43 +0530

    Time: 176 ms (75.00%), Space: 69.9 MB (45.88%) - LeetHub

---
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+        unordered_map<int,int>memo;
+        int n = arr.size();
+        
+        for(int i=0;i<n;i++){
+            int currentVal = arr[i];
+            int currentRem = ((currentVal % k) + k) % k;
+            
+            if(memo.find(currentRem) != memo.end()){
+                memo[currentRem] += 1;
+            }
+            else{
+                memo[currentRem] = 1;
+            }
+        }
+        
+        for(auto key: memo){
+            //Corner Case
+            if(key.first==0){
+                if(memo[key.first]%2!=0){
+                    return false;
+                }
+            }
+            else if(memo.find(k-key.first)==memo.end()){
+                return false;
+            }
+            else if(memo[key.first]!=memo[k-key.first]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+// Time Complexity : O(n) + O(k) for k entries in a hashmap
+// Space Complecity : O(k) for k entries in hashmap
