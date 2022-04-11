8bae6b0f7c9b16b2fa93377d5eff1263928d012a
Aryan Shaw <aryan4p130202@gmail.com>
2022-04-11 10:32:49 +0530

    Added solution - LeetHub

--- Zero Sum Subarrays - GFG/zero-sum-subarrays.cpp
+++ Zero Sum Subarrays - GFG/zero-sum-subarrays.cpp
@@ -16,9 +16,12 @@ class Solution{
         unordered_map<int,int>mp;
         int prefixSum = 0;
         int count = 0;
-        mp[prefixSum] = 1;
         for(int i=0;i<n;i++){
             prefixSum += arr[i];
+            
+            if(prefixSum == 0){
+                count++;
+            }
             if(mp.find(prefixSum)!=mp.end()){
                 count+=mp[prefixSum];
                 mp[prefixSum]+=1;
