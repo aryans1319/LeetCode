3f4371bc1fe4af940a777c0d8d2767c81a1daaad
Aryan Shaw <aryan4p130202@gmail.com>
2022-04-13 12:46:25 +0530

    Time: 145 ms (76.70%), Space: 83.8 MB (83.33%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -5,9 +5,8 @@ public:
         int ans = 0;
         
         unordered_map<int,int>mp;
-        // mp[0]=-1;
+        mp[0]=-1;
         for(int i=0;i<A.size();i++){
-//             prefixSum+=A[i];
             
             if(A[i] == 0){
                 prefixSum+=-1;
@@ -15,17 +14,13 @@ public:
             else {
                 prefixSum+=A[i];
             }
-            if(prefixSum == 0){
-                ans = i+1;
-            }
-            else{
+
             if(mp.find(prefixSum)!=mp.end()){
                 int lastOccIndexLen = i - mp[prefixSum];
                 ans = max(ans,lastOccIndexLen);
             }
             else{
                 mp[prefixSum] = i;
-            }
             }
         }
         return ans;
