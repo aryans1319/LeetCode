cd16efa19a458bb2423ee19cfbe35073b71044f0
Aryan Shaw <aryan4p130202@gmail.com>
2022-04-20 19:18:29 +0530

    Time: 25 ms (20.17%), Space: 15.7 MB (44.26%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -6,19 +6,22 @@ public:
         int start = 0;
         int end = n-1;
         
-        vector<int>ans;
+        vector<int>ans({});
         
         while(start <= end){
-            if(nums[start] + nums[end] > target){
-                end--;
-            }  
-            else if(nums[start] + nums[end] < target){
-                start++;
-            }
-            else{
+            int sum = nums[start] + nums[end];
+            
+            if(sum == target){
                 ans.push_back(start+1);
                 ans.push_back(end+1);
-                break;
+                return ans;
+            }
+            
+            if(sum > target){
+                end--;
+            }  
+            else if(sum < target){
+                start++;
             }
         }
         return ans;
