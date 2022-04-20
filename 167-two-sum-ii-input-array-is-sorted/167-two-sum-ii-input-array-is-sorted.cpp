a5aaf596e916864754bfcc55ff9b31bfe98cb8eb
Aryan Shaw <aryan4p130202@gmail.com>
2022-04-20 19:27:07 +0530

    Time: 24 ms (22.78%), Space: 15.5 MB (92.91%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -8,6 +8,38 @@ public:
         
         vector<int>ans({});
         
+        /* 
+        
+           The idea is very simple since the array is sorted
+           we can use the two pointer technique in order to 
+           solve this problem
+           
+           Keep a start pointer at index = 0 and an end pointer
+           at n-1
+           
+           There can be 3 possible cases:  
+           
+           Case 1 : nums[start] + nums[end] == target 
+           
+           --->     Then we can simply return the indexes + 1 
+                    and we got out target indexes
+           
+           Case 2 : nums[start] + nums[end] > target 
+           
+           --->     if the value is greater than the target
+                    we can say that in order to get our desired 
+                    value we need to reduce the sum, and as we know
+                    the array is sorted in increasing order
+                    we can just reduce the value by decrementing
+                    the end pointer do that end--
+                    
+           Case 3 : nums[start] + nums[end] < target
+           
+           --->     Just reverse of case 2, if value is less than target
+                    we can increment the starting pointer 
+         
+         */           
+        
         while(start <= end){
             int sum = nums[start] + nums[end];
             
