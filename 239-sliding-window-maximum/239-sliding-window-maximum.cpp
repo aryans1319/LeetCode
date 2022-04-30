c7d484a81ab6d39acfdc35789fb6b681aed3a2c4
Aryan Shaw <aryan4p130202@gmail.com>
2022-04-30 16:48:58 +0530

    Time: 546 ms (23.40%), Space: 189.7 MB (11.92%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    vector<int> maxSlidingWindow(vector<int>& a, int k) {
         vector<int>ans;
-        int i = 0;
-        int j = 0;
-        
-        list<int> l;
-        
-        while(j < nums.size()){
-            
-            if(l.empty()){
-                l.push_back(nums[j]);
-            }
-            else{
-                while(!l.empty() && l.back() < nums[j])
-                    l.pop_back();
-                l.push_back(nums[j]);
-            }
-            
-            if(j-i+1 < k){
-                j++;
-            }
-            else if(j-i+1 == k){
-                ans.push_back(l.front());
-                if(nums[i] == l.front()){
-                    l.pop_front();
-                }
-                i++;
-                j++;
-            }
-            
+list<int>l;
+int i=0,j=0;
+while(j<a.size())
+{
+    if (l.empty()){
+        l.push_back(a[j]);
+    }
+    else{
+    while(l.back()<a[j] && !l.empty())
+    {
+        l.pop_back();
+    }
+    l.push_back(a[j]);
+    }
+    if(j-i+1<k)
+    j++;
+
+    else if(j-i+1==k)
+    {
+        ans.push_back(l.front());
+
+        if(l.front()==a[i])
+        {
+            l.pop_front();
         }
-        return ans;
+        i++;
+        j++;
+    }
+}
+return ans;
     }
 };
