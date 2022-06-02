c2ae51bcec4eb50b8640e3318bf5b9dbb64dacec
Aryan Shaw <aryan4p130202@gmail.com>
2022-06-02 15:02:01 +0530

    Time: 14 ms (32.67%), Space: 5.9 MB (42.10%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int fib(int n) {
-       int dp[n+1];
-        if(n<=1){
-            return n;
-        }
+        if(n==0) return 0;
+        if(n==1) return 1;
+        if(n==2) return 1;
         
-       dp[0] = 0;
-       dp[1] = 1;
-        
-        for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        return dp[n];
+        int partialAns1 = fib(n-1);
+        int partialAns2 = fib(n-2);
+        return partialAns1 + partialAns2;
     }
 };
