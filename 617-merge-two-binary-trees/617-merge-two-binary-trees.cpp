01899e8a34bf1c320acd6c0f1683714c0a791326
Aryan Shaw <aryan4p130202@gmail.com>
2022-06-04 02:48:22 +0530

    Time: 65 ms (26.40%), Space: 32.8 MB (35.47%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
@@ -11,36 +11,20 @@
  */
 class Solution {
 public:
+    TreeNode *merge(TreeNode* root1, TreeNode* root2){
+        if(root1 == NULL && root2 == NULL) return NULL;
+        if(root1 == NULL) return root2;
+        if(root2 == NULL) return root1;
+        
+        TreeNode *newNode = new TreeNode(root1->val + root2->val);
+        
+        newNode->left = merge(root1->left,root2->left);
+        newNode->right = merge(root1->right,root2->right);
+        
+        return newNode;
+    }
+
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-        if(root1==NULL) return root2;
-        if(root2==NULL) return root1;
-        if(root1 == NULL && root2 ==NULL) return NULL;
-        
-        queue<pair<TreeNode*,TreeNode*>>q;
-        q.push({root1,root2});
-        
-        while(!q.empty()){
-            auto temp = q.front();
-            q.pop();
-            
-            if(temp.second != NULL){
-                temp.first->val+=temp.second->val;
-                
-                if(temp.first->left == NULL){
-                    temp.first->left = temp.second->left;
-                }
-                else{
-                    q.push({temp.first->left,temp.second->left});
-                }
-                
-                 if(temp.first->right == NULL){
-                    temp.first->right = temp.second->right;
-                }
-                else{
-                    q.push({temp.first->right,temp.second->right});
-                }
-            }
-        }
-        return root1;
+         return merge(root1,root2);
     }
 };
