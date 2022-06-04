e21dba4bd8981ad61cf78a32ff747b3e7288555e
Aryan Shaw <aryan4p130202@gmail.com>
2022-06-04 13:21:58 +0530

    Time: 5 ms (32.67%), Space: 10 MB (44.59%) - LeetHub

--- 965-univalued-binary-tree/965-univalued-binary-tree.cpp
+++ 965-univalued-binary-tree/965-univalued-binary-tree.cpp
@@ -11,40 +11,17 @@
  */
 class Solution {
 public:
-    bool isUnivalTree(TreeNode* root) {
-       if(root == NULL) return true;
-       if(root->left == NULL && root->right == NULL) return true;
+    bool isSame(TreeNode* root, int val){
+        if(root == NULL) return true;
+        if(root->val != val) return false;
         
-       queue<TreeNode*>q;
-       // the first step in every BFS approach push the root node into the queue 
-       q.push(root);
-        while(!q.empty()){
-            
-            int currentSize = q.size();
-            //iterating for current queue
-            for(int i=0;i<currentSize;i++){
+        bool left = isSame(root->left,val);
+        bool right = isSame(root->right,val);
+        
+        return left && right;
+    }
     
-                TreeNode *currentNode = q.front();
-                q.pop();
-        /* made a currentNode pointer pointing to the front element of queue
-       and in each and every step after pushing the children of every node just
-       check if the currentNode value and root value of that particular node are          same if not return false*/
-                if(currentNode->val!=root->val){
-                    return false;
-                }
-                /* if left child exist push into the queue*/
-                
-                if(currentNode->left!=NULL){
-                    q.push(currentNode->left);
-                }
-                 
-                /* if right child exist push into the queue*/
-                
-                 if(currentNode->right!=NULL){
-                    q.push(currentNode->right);
-                }
-            }
-        }
-        return true;
+    bool isUnivalTree(TreeNode* root) {
+        return isSame(root,root->val);
     }
 };
