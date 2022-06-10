27f1fd8e116e7f4368d6ab565c8ba0abbc6f554d
Aryan Shaw <aryan4p130202@gmail.com>
2022-06-10 19:54:19 +0530

    Time: 9 ms (74.95%), Space: 17.2 MB (85.90%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -12,28 +12,38 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-         int ans=0;
-            if(!root)
-               return ans;
-                    queue<pair<TreeNode *,int>>q;
-            q.push({root,0});
-            while(!q.empty())
-            {
-                   int s=q.size();
-                    int last,first;
-                    int mini=q.back().second;
-                    for(int i=0;i<s;i++)
-                    {
-                            int cur_i = q.front().second-mini;
-                            TreeNode *n = q.front().first;
-                            q.pop();
-                            if(i==0) first= cur_i;
-                            if(i==s-1) last=cur_i;
-                            if(n->left) q.push({n->left,cur_i*2});
-                            if(n->right) q.push({n->right,cur_i*2 +1});
-                    }
-                    ans = max(ans,last-first+1);
-            } 
-            return ans;
+        if(root == NULL){
+            return 0;
+        }
+        int ans = 0;
+        queue<pair<TreeNode*,int>> q;
+        q.push({root,0});
+        
+        while(!q.empty()){
+            int size = q.size();
+            int mini = q.front().second;
+            int first,last;
+            for(int i = 0 ; i < size; i++){
+                long long currentMin = q.front().second - mini;
+                TreeNode *node = q.front().first;
+                q.pop();
+                
+                if(i == 0) first = currentMin;
+                if(i == size-1) last = currentMin;
+                
+                if(node->left!=NULL){
+                    q.push({
+                        node->left, currentMin*2+1
+                    });
+                }
+                if(node->right!= NULL){
+                    q.push({
+                        node->right, currentMin*2+2
+                    });
+                }
+            }
+            ans = max(ans,last-first+1);
+        }
+        return ans;
     }
 };
