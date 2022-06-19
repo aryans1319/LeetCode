bf26c9c31340ebb5c1a962ec86be05df539740c4
Aryan Shaw <aryan4p130202@gmail.com>
2022-06-19 11:46:46 +0530

    Time: 6 ms (20.00%), Space: 6.7 MB (60.00%) - LeetHub

--- 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.cpp
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     string greatestLetter(string s) {
+        /* 
+           Make two Frequency array for both smaller case alphabets and
+           capital case respectively 
+        */   
         vector<int> smallerChar(26,0);
         vector<int> capitalChar(26,0);
         
-        string ans ;
+        string ans;
         
         for(auto i : s){
             if(i >= 'a' && i <= 'z'){
@@ -15,6 +19,10 @@ public:
             }
         }
         
+        /* 
+           if Both upper and lower case of a letter is present store the capital letter in
+           string ans as we are traversing from back so it would be the greater char only!
+        */ 
         for(int i=25; i >= 0; i--){
             if(smallerChar[i] && capitalChar[i]){
                 char greatestCapitalLetter = i + 'A';
