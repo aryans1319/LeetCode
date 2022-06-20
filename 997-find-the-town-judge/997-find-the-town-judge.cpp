d73fe7bd55cf44b98a0890233d766782b2fb8814
Aryan Shaw <aryan4p130202@gmail.com>
2022-06-20 11:24:13 +0530

    Time: 284 ms (35.47%), Space: 69 MB (21.57%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -4,17 +4,17 @@ public:
         vector<int>inDegree(n+1,0);
         vector<int>outDegree(n+1,0);
         
-        for(int i=0;i<trust.size();i++){
-            int v1 = trust[i][0];
-            int v2 = trust[i][1];
+        for(auto currentTrust: trust){
+            int a = currentTrust[0];
+            int b = currentTrust[1];
             
-            outDegree[v1]+=1;
-            inDegree[v2]+=1;
+            inDegree[b]+=1;
+            outDegree[a]+=1;
         }
         
-        for(int i=1;i<=n;i++){
-            if(outDegree[i] == 0 && inDegree[i] == n-1){
-                return i;
+        for(int currentPeople = 1; currentPeople <= n; currentPeople++){
+            if(inDegree[currentPeople] == n-1 && outDegree[currentPeople] == 0){
+                return currentPeople;
             }
         }
         return -1;
