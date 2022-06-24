5f7b27fa1b53c38040efb61ce605fe8720652ce0
Aryan Shaw <aryan4p130202@gmail.com>
2022-06-24 10:12:16 +0530

    Time: 13 ms (42.55%), Space: 14.9 MB (42.59%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -10,31 +10,22 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if(l1 == NULL) return l2;
-        if(l2 == NULL) return l1;
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1 == NULL) return list2;
+        if(list2 == NULL) return list1;
         
-        ListNode *newList = new ListNode(-1);
-        ListNode *dummy = newList;
-        ListNode *head1 = l1;
-        ListNode *head2 = l2;
-        while(head1 != NULL && head2!= NULL){
-            if(head1->val > head2->val){
-                dummy->next = head2;
-                head2 = head2->next;
+        if(list1->val > list2->val) swap(list1,list2);
+        
+        ListNode *res = list1;
+        while(list1!=NULL && list2!=NULL){
+            ListNode *temp = NULL;
+            while(list1!=NULL && list1->val <= list2->val){
+                temp = list1;
+                list1 = list1->next;
             }
-            else{
-                dummy->next = head1;
-                head1 = head1->next;
-            }
-            dummy = dummy->next;
+            temp->next = list2;
+            swap(list1,list2);
         }
-        if(head1!=NULL){
-            dummy->next = head1;
-        }
-        if(head2!=NULL){
-            dummy->next = head2;
-        }
-        return newList->next;
+        return res;
     }
 };
