63a56f48e57e442de70ba2ce03a45e4506f9a0e6
Aryan Shaw <aryan4p130202@gmail.com>
2022-06-26 12:43:53 +0530

    Time: 10 ms (66.68%), Space: 11.8 MB (54.87%) - LeetHub

--- 61-rotate-list/61-rotate-list.cpp
+++ 61-rotate-list/61-rotate-list.cpp
@@ -8,6 +8,43 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+/*
+    There can be two cases : 
+    1.) K < length 2.) K >= length
+    
+    if K is greater or equal to the length of the list
+    Suppose for eg : K = 5 and given list is:
+    
+    1->2->3->4->5
+    
+    After 5 rotation it would be the same list only, after 10 also, after
+    15 also i.e list is same if K is a multiple of linkedlist length
+    
+    Now, suppose K = 12
+    
+    The first 10 rotation would give back the same list, we just need to do
+    2 rotations, extra - so can we conclude that we need to do (K % length) no
+    of rotations 12 % 5 i.e 2 rotations , so K = K % len
+    
+    value[] = {2, 4, 7, 8, 9} k = 3 , shift left 
+    
+    Rotate 1: 4 -> 7 -> 8 -> 9 -> 2
+    Rotate 2: 7 -> 8 -> 9 -> 2 -> 4
+    Rotate 3: 8 -> 9 -> 2 -> 4 -> 7
+    
+    break link of 7 point it to NULL , assign last->next (last will be the node till K) to head
+    
+    Steps:
+    
+    1) Find length 
+    2) Traverse upto K, find last node point it to head
+    3) Break link of node after K, point it to NULL
+    
+    TC : O(N) + O(N - (N+K) 
+    SC : O(1)
+ 
+ */
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
