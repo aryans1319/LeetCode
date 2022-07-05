08897163a0a68121974da25ed10473780dc8f7e7
Aryan Shaw <aryan4p130202@gmail.com>
2022-07-05 14:22:01 +0530

    Time: 268 ms (19.08%), Space: 61.3 MB (27.94%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,26 +1,12 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int low = 1, high = nums.size() - 1, cnt;
-        
-        while(low <=  high)
-        {
-            int mid = low + (high - low) / 2;
-            cnt = 0;
-            // cnt number less than equal to mid
-            for(int n : nums)
-            {
-                if(n <= mid)
-                    ++cnt;
+        sort(nums.begin(),nums.end());
+        for(int i  = 0 ; i < nums.size() ; i++){
+            if(nums[i] == nums[i+1]){
+                return nums[i];
             }
-            // binary search on left
-            if(cnt <= mid)
-                low = mid + 1;
-            else
-            // binary search on right
-                high = mid - 1;
-            
         }
-        return low;
+        return -1;
     }
 };
