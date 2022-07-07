3c0b2da318acd420987f4dec9fc54514134e93ec
Aryan Shaw <aryan4p130202@gmail.com>
2022-07-08 02:52:29 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (87.07%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> powerSet ;
-        vector<int>currentSubset;
-        generateAllSubsets(nums,0,currentSubset,powerSet);
-        return powerSet;
-    }
+     void generateAllSubsets(vector<int>&nums, int currentIndex, vector<int>&sub,vector<vector<int>>&allSubsets){
+         if(currentIndex >= nums.size()){
+             allSubsets.push_back(sub);
+             return;
+         }
+         
+         sub.push_back(nums[currentIndex]);
+         generateAllSubsets(nums,currentIndex +1,sub,allSubsets);
+         sub.pop_back();
+         generateAllSubsets(nums,currentIndex +1,sub,allSubsets);
+         return;
+     }
     
-    void generateAllSubsets(vector<int>&nums, int currentIndex, vector<int>&currentSubset,vector<vector<int>>&powerSet){
-        // passed by reference if we pass by value it would give us empty subsets 
-        if(currentIndex >= nums.size()){
-            powerSet.push_back(currentSubset);
-            return;
-        }
-        
-        int currentVal = nums[currentIndex];
-        
-        currentSubset.push_back(currentVal);
-        generateAllSubsets(nums,currentIndex+1,currentSubset,powerSet);
-        
-        currentSubset.pop_back();
-        generateAllSubsets(nums,currentIndex+1,currentSubset,powerSet);
-        
-        return;
-        
+    
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>allSubsets;
+        vector<int>sub;
+        generateAllSubsets(nums,0,sub,allSubsets);
+        return allSubsets;
     }
 };
