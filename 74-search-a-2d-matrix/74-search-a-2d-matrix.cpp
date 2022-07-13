be4703dcab99f57a56642d4adf2ea759c5a9168c
Aryan Shaw <aryan4p130202@gmail.com>
2022-07-13 15:02:06 +0530

    Time: 7 ms (53.15%), Space: 9.6 MB (51.72%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -9,48 +9,48 @@ public:
             if out of bound that means element not present
             
         */
-        int n = matrix.size();
-        int m = matrix[0].size();
+//         int n = matrix.size();
+//         int m = matrix[0].size();
         
-        int i = 0;
-        int j = m-1;
+//         int i = 0;
+//         int j = m-1;
         
-        while(i < n && j >= 0){
-            if(matrix[i][j] == target){
-                return true;
-            }
-            else if(matrix[i][j] > target){
-                j--;
-            }
-            else{
-                i++;
-            }
-        }
-        return false;
-        
-//         int m = matrix.size();
-//         int n = matrix[0].size();
-        
-//         int start = 0;
-//         int end = m*n  - 1;
-        
-//         while(start <= end){
-//             int mid = start + (end-start)/2;
-            
-//             int currentRow = mid/n;
-//             int currentCol = mid%n;
-            
-//             if(matrix[currentRow][currentCol] == target){
+//         while(i < n && j >= 0){
+//             if(matrix[i][j] == target){
 //                 return true;
 //             }
-            
-//             if(matrix[currentRow][currentCol] < target){
-//                 start = mid + 1;
+//             else if(matrix[i][j] > target){
+//                 j--;
 //             }
 //             else{
-//                 end = mid - 1;
+//                 i++;
 //             }
 //         }
 //         return false;
+        
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        int start = 0;
+        int end = m*n  - 1;
+        
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            
+            int currentRow = mid/n;
+            int currentCol = mid%n;
+            
+            if(matrix[currentRow][currentCol] == target){
+                return true;
+            }
+            
+            if(matrix[currentRow][currentCol] < target){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        return false;
     }
 };
