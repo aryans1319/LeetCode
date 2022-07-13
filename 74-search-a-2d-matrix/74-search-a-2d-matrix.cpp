fb74e5ef97db5e2d04d963f3344b2e70ae9c8178
Aryan Shaw <aryan4p130202@gmail.com>
2022-07-13 14:47:21 +0530

    Time: 3 ms (91.42%), Space: 9.6 MB (5.10%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,29 +1,50 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size();
-        int n = matrix[0].size();
         
-        int start = 0;
-        int end = m*n  - 1;
+        //optimal Approach
+        int n = matrix.size();
+        int m = matrix[0].size();
         
-        while(start <= end){
-            int mid = start + (end-start)/2;
-            
-            int currentRow = mid/n;
-            int currentCol = mid%n;
-            
-            if(matrix[currentRow][currentCol] == target){
+        int i = 0;
+        int j = m-1;
+        
+        while(i < n && j >= 0){
+            if(matrix[i][j] == target){
                 return true;
             }
-            
-            if(matrix[currentRow][currentCol] < target){
-                start = mid + 1;
+            else if(matrix[i][j] > target){
+                j--;
             }
             else{
-                end = mid - 1;
+                i++;
             }
         }
         return false;
+        
+//         int m = matrix.size();
+//         int n = matrix[0].size();
+        
+//         int start = 0;
+//         int end = m*n  - 1;
+        
+//         while(start <= end){
+//             int mid = start + (end-start)/2;
+            
+//             int currentRow = mid/n;
+//             int currentCol = mid%n;
+            
+//             if(matrix[currentRow][currentCol] == target){
+//                 return true;
+//             }
+            
+//             if(matrix[currentRow][currentCol] < target){
+//                 start = mid + 1;
+//             }
+//             else{
+//                 end = mid - 1;
+//             }
+//         }
+//         return false;
     }
 };
