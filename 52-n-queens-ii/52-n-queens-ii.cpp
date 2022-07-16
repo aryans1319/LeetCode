d13d7d4db512779a544cb6639fbace3ee3242d43
Aryan Shaw <aryan4p130202@gmail.com>
2022-07-16 14:47:11 +0530

    Time: 11 ms (35.97%), Space: 7 MB (20.96%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -1,19 +1,90 @@
 class Solution {
 public:
     int totalNQueens(int n) {
-	vector<bool> col(n), diag(2*n-1), anti_diag(2*n-1);
-	return solve(col, diag, anti_diag, 0);
-}
-   
-int solve(vector<bool>& col, vector<bool>& diag, vector<bool>& anti_diag, int row) {
-	int n = size(col), count = 0;
-    if(row == n) return 1;
-	for(int column = 0; column < n; column++)           
-		if(!col[column] && !diag[row + column] && !anti_diag[row - column + n - 1]){        // check if (row, column) is safe
-			col[column] = diag[row + column] = anti_diag[row - column + n - 1] = true;      // place at (row, column)
-			count += solve(col, diag, anti_diag, row + 1);                                  // explore for the next row
-			col[column] = diag[row + column] = anti_diag[row - column + n - 1] = false;     // backtrack
-		}                                
-	return count;
-}
+        vector<vector<string>>ans;
+        vector<vector<char>>board(n,vector<char>(n,'.'));
+        nQueens(board,0,n,ans);
+        return ans.size();
+    }
+     private:
+        void nQueens(vector<vector<char>>&board, int currentRow,int n,vector<vector<string>>&ans){
+            /* 
+            creating the base condition, whenever we reach n or the end
+            col we push the stored value/path of queen into the ans
+            */
+            if(currentRow == n){
+                ans.push_back(constructAns(board,n));
+                return;
+            }
+            for(int currentCol = 0; currentCol < n; currentCol++){
+                /* 
+                iterate in the col and check if its a valid cell or not
+                if it's a valid cell place the queen, move to next row
+                and undo the change
+                */
+                if(isValid(board,currentRow,currentCol,n)){
+                    board[currentRow][currentCol] = 'Q';
+                    nQueens(board,currentRow+1,n,ans);
+                    board[currentRow][currentCol] = '.';
+                }
+            
+        }
+        }
+    private:
+        vector<string> constructAns(vector<vector<char>>&board, int n){
+            vector<string>current;
+            for(int i = 0; i<n;i++){
+                string currentString = "";
+                for(int j = 0; j < n; j++){
+                    currentString += board[i][j];
+                }
+                current.push_back(currentString);
+            }
+            return current;
+        }
+    private:
+    bool isValid(vector<vector<char>>&board, int currentRow, int currentCol,int n){
+        return isRowValid(board,currentRow,n) && isColValid(board,currentCol,n) &&
+               isDiagonalValid(board,currentRow,currentCol,n); 
+    }
+    bool isRowValid(vector<vector<char>>&board,int currentRow, int n){
+        for(int currentCol = 0 ;currentCol < n; currentCol++){
+            if(board[currentRow][currentCol] == 'Q')
+                return false;
+        }
+        return true;
+    }
+    bool isColValid(vector<vector<char>>&board,int currentCol, int n){
+        for(int currentRow = 0 ;currentRow < n; currentRow++){
+            if(board[currentRow][currentCol] == 'Q')
+                return false;
+        }
+        return true;
+    }
+    bool isDiagonalValid(vector<vector<char>>&board,int currentRow,int currentCol, int n){
+        //leftUpDiagonal
+        int x = currentRow;
+        int y = currentCol;
+        
+        while(x>=0 && y>=0){
+            if(board[x][y] == 'Q')
+                return false;
+            
+            x-=1;
+            y-=1;
+        }
+        
+        //rightUpDiagonal
+        x = currentRow;
+        y = currentCol;
+        
+        while(x>=0 && y <n){
+            if(board[x][y] == 'Q')
+                return false;
+            
+            x-=1;
+            y+=1;
+        }
+       return true; 
+      }
 };
