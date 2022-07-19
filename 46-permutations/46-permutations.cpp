23b4b4b782812cc11cd8d205c93069916c56c2bd
Aryan Shaw <aryan4p130202@gmail.com>
2022-07-19 21:07:50 +0530

    Time: 3 ms (82.05%), Space: 8 MB (28.14%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-    void printAllPermutations (vector<int>&ds,vector<int>&nums,vector<vector<int>>&ans,int freq[])    {
-        if(ds.size() == nums.size()){
-            ans.push_back(ds);
+    vector<vector<int>>ans;
+    void backtrack(vector<int>&nums, vector<int>&path, vector<bool>&visited){
+        
+        if(path.size() == nums.size()){
+            ans.push_back(path);
             return;
         }
+        for(int i = 0; i < nums.size();i++){
+            if(visited[i] == false){
+                visited[i] = true;
+                path.push_back(nums[i]);
+                backtrack(nums,path,visited);
+                path.pop_back();
+                visited[i] = false;
+            }
+        }
+    }
         
-        for(int i=0; i<nums.size();i++){
-            if(!freq[i]){
-                ds.push_back(nums[i]);
-                freq[i]=1;
-                printAllPermutations(ds,nums,ans,freq);
-                freq[i]=0;
-                ds.pop_back();
-            }   
-        }    
-   }
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int>ds;
-        int n = nums.size();
-        int freq[n] ;
-        for(int i=0;i<nums.size();i++)freq[i]=0;
-        printAllPermutations(ds,nums,ans,freq);
+        vector<int>path;
+        vector<bool> visited(nums.size(),false);
+        backtrack(nums,path,visited);
         return ans;
     }
 };
