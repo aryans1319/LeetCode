04c672c7b6ca98ad3b1c8d7cc231a9f21983bff5
Aryan Shaw <aryan4p130202@gmail.com>
2022-07-31 02:05:54 +0530

    Time: 1633 ms (29.65%), Space: 18.7 MB (54.40%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.py
@@ -0,0 +1,7 @@
+class Solution:
+    def wordSubsets(self, A, B):
+        count = collections.Counter()
+        for b in B:
+            count |= collections.Counter(b)
+        return [a for a in A if not count - Counter(a)]
+        
