a68f1dead7867bd168aea3cef94f91e927049632
Aryan Shaw <aryan4p130202@gmail.com>
2022-07-31 01:26:36 +0530

    Time: 3 ms (88.63%), Space: 11.1 MB (75.10%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int start = 0;
-        int end = nums.size()-1;
+    int binarySearch(vector<int>& nums, int target, int l,int r) {
+        int start = l;
+        int end = r;
         
         while(start <= end){
             int mid = start + (end-start)/2;
@@ -11,27 +11,52 @@ public:
                 return mid;
             }
             
-            if(nums[mid] >= nums[start]){
-                
-                if(nums[start] <= target && nums[mid] > target){
-                    end = mid-1;
-                }
-                else{
-                    start = mid+1;
-                }
-                
+            else if(nums[mid] < target){
+                start = mid+1;
             }
             else{
-                
-                if(nums[mid] < target && nums[end] >= target){
-                    start = mid+1;
-                }
-                else{
-                    end = mid-1;
-                }
-                
+                end = mid-1;
             }
         }
         return -1;
+    }
+    
+    int findMin(vector<int>& nums) {
+        int start = 0, end = nums.size()-1;
+        int len = nums.size(); 
+        
+        if(nums[start] <= nums[end]){
+            return nums[start];
+        }
+        
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            int next = ((mid + len - 1)%len);
+            
+            if(nums[mid] <= nums[next]){
+                return mid;
+            }
+            else if(nums[mid] >= nums[0]){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        
+        return 0;
+    }
+    
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int minIndex = findMin(nums);
+        cout<<minIndex<<" ";
+        int a = binarySearch(nums,target,0,minIndex-1);
+        int b = binarySearch(nums,target,minIndex,n-1);
+        
+        if(a == -1)
+            return b;
+        else
+            return a; 
     }
 };
