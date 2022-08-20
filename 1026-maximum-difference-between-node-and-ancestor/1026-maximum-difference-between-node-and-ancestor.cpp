7170dea8b46e760b81cde3ed7a114efca86ad28b
Aryan Shaw <aryan4p130202@gmail.com>
2022-08-20 10:50:16 +0530

    Time: 11 ms (38.81%), Space: 9.6 MB (96.88%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
@@ -9,58 +9,29 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// class Solution {
-// public:
-//     int maximumDifference = INT_MIN;
-    
-//     void findMaxDiff(TreeNode *root, int maxi, int mini){
-//         if (root == NULL) return;
-        
-//         maxi = max(maxi, root->val);
-//         mini = min(mini, root->val);
-        
-//         maximumDifference = max(maximumDifference, maxi - mini);
-        
-//         findMaxDiff(root->left, maxi,mini);
-//         findMaxDiff(root->left, maxi,mini);
-        
-//     }
-    
-//     int maxAncestorDiff(TreeNode* root) {
-//         if(!root) return 0;
-//         int maxi = INT_MIN;
-//         int mini = INT_MAX;
-        
-//         findMaxDiff(root,maxi,mini);
-//         return maximumDifference;
-//     }
-// };
 class Solution {
 public:
+    int maximumDifference = INT_MIN;
     
-    int diff = INT_MIN;
-    
-    void getMax(TreeNode *root, int mx, int mi){
+    void findMaxDiff(TreeNode *root, int maxi, int mini){
+        if (root == NULL) return;
         
-        if(root == NULL) return;
-       
-        mx = max(mx, root->val);
-        mi = min(mi, root->val);
+        maxi = max(maxi, root->val);
+        mini = min(mini, root->val);
         
-        diff = max(diff, mx - mi);
+        maximumDifference = max(maximumDifference, maxi - mini);
         
-        getMax(root->left,mx,mi);
-        getMax(root->right,mx,mi);
+        findMaxDiff(root->left, maxi,mini);
+        findMaxDiff(root->right, maxi,mini);
+        
     }
     
     int maxAncestorDiff(TreeNode* root) {
-        
         if(!root) return 0;
+        int maxi = INT_MIN;
+        int mini = INT_MAX;
         
-        int mx = INT_MIN, mi = INT_MAX;
-
-        getMax(root,mx,mi);
-        
-        return diff;
+        findMaxDiff(root,maxi,mini);
+        return maximumDifference;
     }
 };
