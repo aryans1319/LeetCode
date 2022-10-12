e7e5bd99642ffb564887f263781709165cc950ae
Aryan Shaw <aryan4p130202@gmail.com>
2022-10-13 01:48:37 +0530

    Time: 27 ms (42.69%), Space: 10 MB (19.34%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,34 +1,35 @@
 class Solution {
-private:
-    bool isValid(int x, int y, int m, int n, vector <vector <int>>&grid, vector <vector <int>>&visited){
-        if(visited[x][y] or grid[x][y]) return false;
-        return true;
-    }
-    bool dfs(int row, int col, vector<vector<int>>&vis, vector<vector<int>>&grid) {
-        if(!isValid(row, col, grid.size(), grid[0].size(), grid, vis)) return true; 
-        
-        if(row == grid.size()-1 || row == 0 || col == grid[0].size()-1 || col == 0){
-              return false;
-        }   
-        vis[row][col] = 1;
-        bool left = dfs(row, col - 1, vis, grid);
-        bool right = dfs(row, col + 1, vis, grid);
-        bool down = dfs(row + 1, col, vis, grid);
-        bool up = dfs(row - 1, col, vis, grid);
-        return left && right && down && up;
-    }
 public:
+     bool dfs(int row, int col, vector<vector<int>>&vis, vector<vector<int>>&grid, int del_row[], int del_col[]) {
+        if(vis[row][col]) return 1;
+        vis[row][col] = 1;
+        bool isClosed = true;
+        for(int i = 0; i < 4; i++){
+            int nrow = row + del_row[i];
+            int ncol = col + del_col[i];
+            
+            if(nrow >=0 && nrow < grid.size() && ncol >= 0 && ncol < grid[0].size() && grid[nrow][ncol] == 0){
+                if(nrow == 0 || nrow == grid.size() - 1 || ncol == 0 || ncol == grid[0].size() - 1){
+                    isClosed = false;
+                }
+                else{
+                    isClosed = dfs(nrow, ncol, vis, grid,del_row, del_col) && isClosed;
+                }
+            }
+        } 
+        return isClosed;
+    }
     int closedIsland(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
         int islands = 0;
+        int del_row[4] = {-1,0,+1,0};
+        int del_col[4] = {0,1,0,-1};
         vector<vector<int>>vis(n, vector<int>(m,0));
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(!vis[i][j] && grid[i][j] == 0){
-                    if(dfs(i,j,vis,grid)){
+        for(int i = 1; i < n-1; i++){
+            for(int j = 1; j < m-1; j++){
+                if(!vis[i][j] && grid[i][j] == 0 && dfs(i,j,vis,grid,del_row,del_col)){
                         islands++;
-                    }
                 }
             }
         }
