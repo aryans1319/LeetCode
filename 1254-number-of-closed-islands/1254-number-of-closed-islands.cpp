5f1ae7e0e729b9e6399c5f0200ca09f537d6df22
Aryan Shaw <aryan4p130202@gmail.com>
2022-10-13 02:25:14 +0530

    Time: 28 ms (38.04%), Space: 10.1 MB (19.34%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-     bool bfs(int row, int col, vector<vector<int>>&vis, vector<vector<int>>&grid, int del_row[], int del_col[]) {
-        // if(vis[row][col]) return 1;
-        queue<pair<int,int>>q;
-        q.push({row,col}); 
+     bool dfs(int row, int col, vector<vector<int>>&vis, vector<vector<int>>&grid, int del_row[], int del_col[]) {
+        if(vis[row][col]) return 1;
         vis[row][col] = 1;
         bool isClosed = true;
-        while(!q.empty()){
-            int r = q.front().first;
-            int c = q.front().second;
-            q.pop();
-            for(int i = 0; i < 4; i++){
-                int nrow = r + del_row[i];
-                int ncol = c + del_col[i];
-                 if(!(nrow >=0 && nrow < grid.size() && ncol >= 0 && ncol < grid[0].size())){
-                     isClosed = false;
-                 }
-                
-                if(nrow >=0 && nrow < grid.size() && ncol >= 0 && ncol < grid[0].size() && grid[nrow][ncol] == 0 && vis[nrow][ncol] == 0){
-                        q.push({nrow, ncol});
-                        vis[nrow][ncol] = 1;
-                     
-                    }
+        for(int i = 0; i < 4; i++){
+            int nrow = row + del_row[i];
+            int ncol = col + del_col[i];
+            
+            if(nrow >=0 && nrow < grid.size() && ncol >= 0 && ncol < grid[0].size() && grid[nrow][ncol] == 0 && vis[nrow][ncol] == 0){
+                if(nrow == 0 || nrow == grid.size() - 1 || ncol == 0 || ncol == grid[0].size() - 1){
+                    isClosed = false;
+                }
+                else{
+                    isClosed = dfs(nrow, ncol, vis, grid,del_row, del_col) && isClosed;
                 }
             }
+        } 
         return isClosed;
     }
     int closedIsland(vector<vector<int>>& grid) {
@@ -35,7 +28,7 @@ public:
         vector<vector<int>>vis(n, vector<int>(m,0));
         for(int i = 1; i < n-1; i++){
             for(int j = 1; j < m-1; j++){
-                if(vis[i][j] == 0 && grid[i][j] == 0 &&  bfs(i,j,vis,grid,del_row,del_col)){
+                if(!vis[i][j] && grid[i][j] == 0 && dfs(i,j,vis,grid,del_row,del_col)){
                         islands++;
                 }
             }
