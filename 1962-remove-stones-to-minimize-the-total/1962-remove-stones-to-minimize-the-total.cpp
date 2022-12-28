8c64ba0272a8760443f319757658c6ef7ca805ed
Aryan Shaw <aryan4p130202@gmail.com>
2022-12-28 13:52:04 +0530

    Time: 773 ms (63.62%), Space: 104.9 MB (55.34%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
@@ -1,6 +1,35 @@
 class Solution {
 public:
     int minStoneSum(vector<int>& piles, int k) {
+        /*
+            The problem given is to find the minimum possible
+            sum after performing K number of operations on a particular
+            element given in the array
+            
+            the operation - remove floor(a[i]/2) from any given a[i]
+            
+            By observation we can say
+                - To get minimum sum we need to reduce the maximum element
+                - To get max element in every iteration
+                - We can use some data structure to get max element in every 
+                  iteration hence we get an idea of using max heap
+            
+            We can do at max K operations so we iterate for K times and
+            in every iteration take top element (max element) and reduce it
+            by element/2 and repush into the queue
+            
+            After K iteration return sum of all elements of the priority queue thus
+            we would get the minimum sum
+            
+            Time Complexity : O(n) for creating priority queue
+                              O(k) for running upto K iterations, 
+                                   insertion and deletion in pq takes O(logn) time
+                                  
+                                   So, traversing and deleting simultaneously takes
+                                   O(nlogn)
+                                   
+                                   total = O((n + k) logn)
+        */
         priority_queue<int> pq;
         int sum = 0;
         for(auto i : piles){
