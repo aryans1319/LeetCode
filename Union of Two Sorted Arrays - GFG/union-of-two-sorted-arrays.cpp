988d0df6ad6ef21bbd2afc7befcf171d00502103
Aryan Shaw <aryan4p130202@gmail.com>
2023-01-02 15:10:45 +0530

    Added solution - LeetHub

---
+++ Union of Two Sorted Arrays - GFG/union-of-two-sorted-arrays.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //arr1,arr2 : the arrays
+    // n, m: size of arrays
+    //Function to return a list containing the union of the two arrays. 
+    vector<int> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+        set<int>st;
+        vector<int>ans;
+        for(int i = 0; i < n; i++){
+            st.insert(arr1[i]);
+        }
+        for(int i = 0; i < m; i++){
+            st.insert(arr2[i]);
+        }
+        for(auto j : st){
+            ans.push_back(j);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    
+	    
+	    
+	    int N, M;
+	    cin >>N >> M;
+	    
+	    int arr1[N];
+	    int arr2[M];
+	    
+	    for(int i = 0;i<N;i++){
+	        cin >> arr1[i];
+	    }
+	    
+	    for(int i = 0;i<M;i++){
+	        cin >> arr2[i];
+	    }
+	    Solution ob;
+	    vector<int> ans = ob.findUnion(arr1,arr2, N, M);
+	    for(int i: ans)cout<<i<<' ';
+	    cout << endl;
+	    
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
